da02d4dede92a7afa6baf55933a1fb193243f38a
kapeka <kapeka@users.sourceforge.net>
2011-02-07 15:30:09 +0100

    add package source patch to git

---
+++ bering-uclibc4/source/patch/buildtool.mk
@@ -0,0 +1,34 @@
+include $(MASTERMAKEFILE)
+PATCH_DIR:=patch-2.5.9
+PATCH_TARGET_DIR:=$(BT_BUILD_DIR)/patch
+
+$(PATCH_DIR)/.source: 
+	zcat $(PATCH_SOURCE) |  tar -xvf - 
+	touch $(PATCH_DIR)/.source
+
+$(PATCH_DIR)/.configured: $(PATCH_DIR)/.source
+	(cd $(PATCH_DIR); CC=$(TARGET_CC) LD=$(TARGET_LD) CFLAGS="$(BT_COPT_FLAGS)" \
+	./configure --prefix=/usr)
+	touch $(PATCH_DIR)/.configured
+
+$(PATCH_DIR)/.build: $(PATCH_DIR)/.configured
+	mkdir -p $(PATCH_TARGET_DIR)/usr/bin
+	$(MAKE) -C $(PATCH_DIR)
+	cp -a $(PATCH_DIR)/patch $(PATCH_TARGET_DIR)/usr/bin
+	$(BT_STRIP) $(BT_STRIP_BINOPTS) $(PATCH_TARGET_DIR)/usr/bin/patch
+	cp -a $(PATCH_TARGET_DIR)/* $(BT_STAGING_DIR)/
+	touch $(PATCH_DIR)/.build
+
+source: $(PATCH_DIR)/.source 
+
+build: $(PATCH_DIR)/.build
+
+clean:
+	$(MAKE) -C $(PATCH_DIR) clean
+	-rm -rf $(PATCH_TARGET_DIR)
+	-rm $(BT_STAGING_DIR)/usr/sbin/patch
+	-rm $(PATCH_DIR)/.build
+	-rm $(PATCH_DIR)/.configured
+
+srcclean:
+	rm -rf $(PATCH_DIR)
