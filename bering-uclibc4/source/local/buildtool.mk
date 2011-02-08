7060743fe5691549a8be29f5c9d04e5367e5dbba
kapeka <kapeka@users.sourceforge.net>
2011-02-08 21:52:15 +0100

    move local to git

---
+++ bering-uclibc4/source/local/buildtool.mk
@@ -0,0 +1,16 @@
+#############################################################
+#
+# local
+#
+#############################################################
+
+include $(MASTERMAKEFILE)
+
+source: 
+	# nothing to be done
+
+build:
+	# nothing to be done
+
+clean:
+	# nothing to be done
