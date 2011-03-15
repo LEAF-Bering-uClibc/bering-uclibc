89636f6c9a410960ec6bff1ffdd2ee107447ea85
kapeka <kapeka@users.sourceforge.net>
2011-03-15 10:44:05 +0100

    new upstream version 2.57
    Currently I have not enabled IDN support.

---
+++ bering-uclibc4/buildtool/repo/dnsmasq/buildtool.mk
@@ -0,0 +1,44 @@
+# makefile for dnsmasq
+include $(MASTERMAKEFILE)
+
+DNSMASQ_DIR:=dnsmasq-2.57
+DNSMASQ_TARGET_DIR:=$(BT_BUILD_DIR)/dnsmasq
+
+$(DNSMASQ_DIR)/.source:
+	zcat $(DNSMASQ_SOURCE) | tar -xvf -
+	touch $(DNSMASQ_DIR)/.source
+
+source: $(DNSMASQ_DIR)/.source
+                        
+$(DNSMASQ_DIR)/.configured: $(DNSMASQ_DIR)/.source
+	touch $(DNSMASQ_DIR)/.configured
+                                                                 
+$(DNSMASQ_DIR)/.build: $(DNSMASQ_DIR)/.configured
+	mkdir -p $(DNSMASQ_TARGET_DIR)
+	mkdir -p $(DNSMASQ_TARGET_DIR)/usr/sbin	
+	mkdir -p $(DNSMASQ_TARGET_DIR)/etc/init.d	
+	# Disable the internal tftpd with -DNO_TFTP
+	make -C $(DNSMASQ_DIR) DESTDIR=$(DNSMASQ_TARGET_DIR) \
+	CC=$(TARGET_CC) LD=$(TARGET_LD) CFLAGS="$(BT_COPT_FLAGS) -DNO_TFTP" all  
+	-$(BT_STRIP) -s --remove-section=.note --remove-section=.comment $(DNSMASQ_DIR)/src/dnsmasq	
+	# Adjust the dnsmasq.conf to LEAF standard
+	perl -i -p -e 's,\#dhcp-range=192.168.0.50\,192.168.0.150\,12h,\#dhcp-range=192.168.1.1\,192.168.1.199\,12h,g' $(DNSMASQ_DIR)/dnsmasq.conf.example
+	perl -i -p -e 's,\#domain=thekelleys.org.uk,domain=private.network,g' $(DNSMASQ_DIR)/dnsmasq.conf.example
+	perl -i -p -e 's,\#local=/localnet/,local=/private.network/,g' $(DNSMASQ_DIR)/dnsmasq.conf.example
+	cp -aL dnsmasq.init $(DNSMASQ_TARGET_DIR)/etc/init.d/dnsmasq
+	cp -a $(DNSMASQ_DIR)/src/dnsmasq $(DNSMASQ_TARGET_DIR)/usr/sbin
+	cp -a $(DNSMASQ_DIR)/dnsmasq.conf.example $(DNSMASQ_TARGET_DIR)/etc/dnsmasq.conf
+	cp -a $(DNSMASQ_TARGET_DIR)/* $(BT_STAGING_DIR)
+	touch $(DNSMASQ_DIR)/.build
+
+build: $(DNSMASQ_DIR)/.build
+                                                                                         
+clean:
+	make -C $(DNSMASQ_DIR) clean
+	rm -rf $(DNSMASQ_TARGET_DIR)
+	rm $(DNSMASQ_DIR)/.build
+	rm $(DNSMASQ_DIR)/.configured
+                                                                                                                 
+srcclean: clean
+	rm -rf $(DNSMASQ_DIR) 
+	rm $(DNSMASQ_DIR)/.source
