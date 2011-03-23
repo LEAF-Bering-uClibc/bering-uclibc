dc8a9e8aaeb976bf64b237019bb58b7bbab03ac1
kapeka <kapeka@users.sourceforge.net>
2011-03-23 17:46:46 +0100

    new upstream version 0.95

---
+++ bering-uclibc4/buildtool/repo/p9100/buildtool.mk
@@ -0,0 +1,38 @@
+# makefile for p9100
+include $(MASTERMAKEFILE)
+
+P9100_DIR:=p910nd-0.95
+P9100_TARGET_DIR:=$(BT_BUILD_DIR)/p9100
+
+$(P9100_DIR)/.source:
+	bzcat $(P9100_SOURCE) | tar -xvf -
+	touch $(P9100_DIR)/.source
+
+source: $(P9100_DIR)/.source
+                        
+$(P9100_DIR)/.configured: $(P9100_DIR)/.source
+	touch $(P9100_DIR)/.configured
+                                                                 
+$(P9100_DIR)/.build: $(P9100_DIR)/.configured
+	mkdir -p $(P9100_TARGET_DIR)
+	mkdir -p $(P9100_TARGET_DIR)/etc/init.d	
+	mkdir -p $(P9100_TARGET_DIR)/etc/default
+	mkdir -p $(P9100_TARGET_DIR)/usr/sbin	
+	make CC=$(TARGET_CC) -C $(P9100_DIR) CFLAGS="$(BT_COPT_FLAGS) -DLOCKFILE_DIR=\\"\"/var/lock\\"\""
+	-$(BT_STRIP) $(BT_STRIP_BINOPTS) $(P9100_DIR)/p910nd
+	cp -a $(P9100_DIR)/p910nd $(P9100_TARGET_DIR)/usr/sbin	
+	cp -aL p910nd.init $(P9100_TARGET_DIR)/etc/init.d/p910nd
+	cp -aL p910nd.default $(P9100_TARGET_DIR)/etc/default/p910nd
+	cp -a $(P9100_TARGET_DIR)/* $(BT_STAGING_DIR)
+	touch $(P9100_DIR)/.build
+
+build: $(P9100_DIR)/.build
+                                                                                         
+clean:
+	rm -rf $(P9100_TARGET_DIR)
+	rm $(P9100_DIR)/p910nd
+	rm $(P9100_DIR)/.build
+	rm $(P9100_DIR)/.configured
+                                                                                                                 
+srcclean:
+	rm -rf $(P9100_DIR) 
