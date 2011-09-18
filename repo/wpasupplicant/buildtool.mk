34b8beb7a8a146977e34db6ed3e38f22db1f125f
Erich Titl <erich.titl@think.ch>
2011-09-19 00:08:25 +0200

    configuration for wpa_supplicant 0.7.3

--- repo/wpasupplicant/buildtool.mk
+++ repo/wpasupplicant/buildtool.mk
@@ -1,46 +1,46 @@
 # makefile for wpasupplicant
 include $(MASTERMAKEFILE)
 
-WPA_DIR:=wpa_supplicant-0.5.11
-WPA_TARGET_DIR:=$(BT_BUILD_DIR)/wpasupplicant
+SOURCE_DIR:=$(shell basename `tar tzf $(SOURCE_TARBALL) | head -1`)/wpa_supplicant
+TARGET_DIR:=$(BT_BUILD_DIR)/wpasupplicant
 
 
-$(WPA_DIR)/.source:
-	zcat $(WPA_SOURCE) | tar -xvf -
-#	cat $(WPA_PATCH) | patch -d $(WPA_DIR) -p1
-	touch $(WPA_DIR)/.source
+.source:
+	zcat $(SOURCE_TARBALL) | tar -xvf -
+	touch .source
 
-source: $(WPA_DIR)/.source
+source: .source
+
                         
-$(WPA_DIR)/.build:
-	mkdir -p $(WPA_TARGET_DIR)
-	mkdir -p $(WPA_TARGET_DIR)/usr/sbin
-	mkdir -p $(WPA_TARGET_DIR)/etc
-	mkdir -p $(WPA_TARGET_DIR)/etc/default
-	mkdir -p $(WPA_TARGET_DIR)/etc/init.d
-	cp -aL .config $(WPA_DIR)/
-	( cd $(WPA_DIR) ; export CFLAGS="$(BT_COPT_FLAGS) -MMD -Wall -g -I$(BT_STAGING_DIR)/usr/include" ; \
+.build: 
+	mkdir -p $(TARGET_DIR)
+	mkdir -p $(TARGET_DIR)/usr/sbin
+	mkdir -p $(TARGET_DIR)/etc
+	mkdir -p $(TARGET_DIR)/etc/default
+	mkdir -p $(TARGET_DIR)/etc/init.d
+	cp -aL .config $(SOURCE_DIR)/.config
+	( cd $(SOURCE_DIR) ; export CFLAGS="$(BT_COPT_FLAGS) -MMD -Wall -g -I$(BT_STAGING_DIR)/usr/include" ; \
 	export LIBS=-L$(BT_STAGING_DIR)/usr/lib ; make CC=$(TARGET_CC) ) ;
-	-$(BT_STRIP) $(BT_STRIP_BINOPTS) $(WPA_DIR)/wpa_cli
-	-$(BT_STRIP) $(BT_STRIP_BINOPTS) $(WPA_DIR)/wpa_passphrase
-	-$(BT_STRIP) $(BT_STRIP_BINOPTS) $(WPA_DIR)/wpa_supplicant
-	cp -a $(WPA_DIR)/wpa_cli $(WPA_TARGET_DIR)/usr/sbin	
-	cp -a $(WPA_DIR)/wpa_passphrase $(WPA_TARGET_DIR)/usr/sbin	
-	cp -a $(WPA_DIR)/wpa_supplicant $(WPA_TARGET_DIR)/usr/sbin	
-	cp -aL wpa_supplicant.conf $(WPA_TARGET_DIR)/etc	
-	cp -aL wpasupplicant.default $(WPA_TARGET_DIR)/etc/default/wpasupplicant
-	cp -aL wpasupplicant.init $(WPA_TARGET_DIR)/etc/init.d/wpasupplicant
-	cp -a $(WPA_TARGET_DIR)/* $(BT_STAGING_DIR)
-	touch $(WPA_DIR)/.build
+	-$(BT_STRIP) $(BT_STRIP_BINOPTS) $(SOURCE_DIR)/wpa_cli
+	-$(BT_STRIP) $(BT_STRIP_BINOPTS) $(SOURCE_DIR)/wpa_passphrase
+	-$(BT_STRIP) $(BT_STRIP_BINOPTS) $(SOURCE_DIR)/wpa_supplicant
+	cp -a $(SOURCE_DIR)/wpa_cli $(TARGET_DIR)/usr/sbin	
+	cp -a $(SOURCE_DIR)/wpa_passphrase $(TARGET_DIR)/usr/sbin	
+	cp -a $(SOURCE_DIR)/wpa_supplicant $(TARGET_DIR)/usr/sbin	
+	cp -aL wpa_supplicant.conf $(TARGET_DIR)/etc	
+	cp -aL wpasupplicant.default $(TARGET_DIR)/etc/default/wpasupplicant
+	cp -aL wpasupplicant.init $(TARGET_DIR)/etc/init.d/wpasupplicant
+	cp -a $(TARGET_DIR)/* $(BT_STAGING_DIR)
+	touch .build
 
-build: $(WPA_DIR)/.build
+build: .build
                                                                                          
 clean:
-	make -C $(WPA_DIR) clean
-	rm -rf $(WPA_TARGET_DIR)
-	rm -f $(WPA_DIR)/.build
-	rm -f $(WPA_DIR)/.configured
+	make -C $(SOURCE_DIR) clean
+	rm -rf $(TARGET_DIR)
+	rm -f .build
                                                                                                                  
 srcclean: clean
-	rm -rf $(WPA_DIR) 
-	rm -f $(WPA_DIR)/.source
+	rm -rf $(SOURCE_DIR) 
+	rm -f .source
+	rm -f .configured
