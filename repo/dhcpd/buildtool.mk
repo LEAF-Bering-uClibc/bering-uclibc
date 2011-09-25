f955d73ac7c4daa94ca8ffb94d3463ca2d99fbf8
Erich Titl <erich.titl@think.ch>
2011-09-25 23:32:22 +0200

    my attempt to update dhcpd... whatever

--- repo/dhcpd/buildtool.mk
+++ repo/dhcpd/buildtool.mk
@@ -1,52 +1,53 @@
 # makefile for dhcpd
 include $(MASTERMAKEFILE)
 
-DHCPD_DIR:=dhcp-2.0pl5
-DHCPD_TARGET_DIR:=$(BT_BUILD_DIR)/dhcpd
+SOURCE_TARBALL=dhcp-4.2.2.tar.gz
+SOURCE_DIR:=$(shell basename `tar tzf $(SOURCE_TARBALL) | head -1`)
+TARGET_DIR:=$(BT_BUILD_DIR)/dhcpd
 
-$(DHCPD_DIR)/.source:
-	zcat $(DHCPD_SOURCE) | tar -xvf -
-	zcat $(DHCPD_PATCH1) | patch -d $(DHCPD_DIR) -p1
-	touch $(DHCPD_DIR)/.source
+.source:
+	zcat $(SOURCE_TARBALL) | tar -xvf -
+	#zcat $(DHCPD_PATCH1) | patch -d $(SOURCE_DIR) -p1
+	touch $(SOURCE_DIR)/.source
 
 
-unpatch:
-	if [ -f $(DHCPD_DIR)/.patched ]; then ( cd $(DHCPD_DIR) && /bin/sh debian/scripts/unpatch-source ); fi
-	rm -f $(DHCPD_DIR)/.patched
+#unpatch:
+#	if [ -f .patched ]; then ( cd $(SOURCE_DIR) && /bin/sh debian/scripts/unpatch-source ); fi
+#	rm -f .patched
 
-$(DHCPD_DIR)/.patched: $(DHCPD_DIR)/.source
-	( cd $(DHCPD_DIR) && /bin/sh debian/scripts/patch-source );
-	touch $(DHCPD_DIR)/.patched
+#.patched: .source
+#	( cd $(SOURCE_DIR) && /bin/sh debian/scripts/patch-source );
+#	touch .patched
  
-source: $(DHCPD_DIR)/.source
+source: .source
                         
-$(DHCPD_DIR)/.configured: $(DHCPD_DIR)/.patched
-	(cd $(DHCPD_DIR) ; LD=$(TARGET_LD) ./configure )
-	touch $(DHCPD_DIR)/.configured
+.configured: .source
+	(cd $(SOURCE_DIR) ; LD=$(TARGET_LD) ./configure )
+	touch .configured
                                                                  
-$(DHCPD_DIR)/.build: $(DHCPD_DIR)/.configured
-	mkdir -p $(DHCPD_TARGET_DIR)
-	mkdir -p $(DHCPD_TARGET_DIR)/usr/sbin
-	mkdir -p $(DHCPD_TARGET_DIR)/etc/init.d
-	mkdir -p $(DHCPD_TARGET_DIR)/etc/default
-	make -C $(DHCPD_DIR) CC=$(TARGET_CC) PREDEFINES='-D_PATH_DHCPD_DB=\"/var/lib/dhcp/dhcpd.leases\" \
+.build: .configured
+	mkdir -p $(TARGET_DIR)
+	mkdir -p $(TARGET_DIR)/usr/sbin
+	mkdir -p $(TARGET_DIR)/etc/init.d
+	mkdir -p $(TARGET_DIR)/etc/default
+	make -C $(SOURCE_DIR) CC=$(TARGET_CC) PREDEFINES='-D_PATH_DHCPD_DB=\"/var/lib/dhcp/dhcpd.leases\" \
 	-D_PATH_DHCLIENT_DB=\"/var/lib/dhcp/dhclient.leases\"' VARDB=/var/lib/dhcp
-	-$(BT_STRIP) -s --remove-section=.note --remove-section=.comment $(DHCPD_DIR)/server/dhcpd
-	cp -a $(DHCPD_DIR)/server/dhcpd $(DHCPD_TARGET_DIR)/usr/sbin
-	cp -aL dhcpd.conf $(DHCPD_TARGET_DIR)/etc	
-	cp -aL dhcp $(DHCPD_TARGET_DIR)/etc/default	
-	cp -aL dhcp.init $(DHCPD_TARGET_DIR)/etc/init.d/dhcp			
-	cp -a $(DHCPD_TARGET_DIR)/* $(BT_STAGING_DIR)
-	touch $(DHCPD_DIR)/.build
+	-$(BT_STRIP) -s --remove-section=.note --remove-section=.comment $(SOURCE_DIR)/server/dhcpd
+	cp -a $(SOURCE_DIR)/server/dhcpd $(TARGET_DIR)/usr/sbin
+	cp -aL dhcpd.conf $(TARGET_DIR)/etc	
+	cp -aL dhcp $(TARGET_DIR)/etc/default	
+	cp -aL dhcp.init $(TARGET_DIR)/etc/init.d/dhcp			
+	cp -a $(TARGET_DIR)/* $(BT_STAGING_DIR)
+	touch .build
 
-build: $(DHCPD_DIR)/.build
+build: .build
                                                                                          
-clean: unpatch
-	make -C $(DHCPD_DIR) distclean
-	rm -rf $(DHCPD_TARGET_DIR)
-	rm $(DHCPD_DIR)/.build
-	rm $(DHCPD_DIR)/.configured
+clean: 
+	make -C $(SOURCE_DIR) distclean
+	rm -rf $(TARGET_DIR)
+	rm .build
+	rm .configured
                                                                                                                  
 srcclean: clean
-	rm -rf $(DHCPD_DIR) 
-	rm $(DHCPD_DIR)/.source
+	rm -rf $(SOURCE_DIR) 
+	rm .source
