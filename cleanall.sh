b75a73b17b4d88c16ea0a1f4b2a523abd2b67fa0
Andrew Denisenko <nitr0man@users.sourceforge.net>
2011-10-31 16:09:00 +0200

    modified cleanann script

--- cleanall.sh
+++ cleanall.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-rm -rf package source staging log build conf/installed
+rm -rf package source staging toolchain log build conf/installed
 find image/ -name *.gz -delete
 find image/ -name *.iso -delete
