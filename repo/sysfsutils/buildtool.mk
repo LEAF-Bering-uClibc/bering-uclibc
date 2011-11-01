717ac7eb313bb71f0e862120e275f428e615ed3b
Andrew Denisenko <nitr0man@users.sourceforge.net>
2011-11-02 00:21:05 +0200

    fixed sysfsutils

--- repo/sysfsutils/buildtool.mk
+++ repo/sysfsutils/buildtool.mk
@@ -16,16 +16,18 @@ source:	$(DIR)/.source
 
 $(DIR)/.build: $(DIR)/.source
 	mkdir -p $(BT_BUILD_DIR)/module-init-tools
-	(cd $(DIR); \
-		rm -rf config.cache; \
-		./configure --prefix=/ --host=$(GNU_TARGET_NAME) );
+	(cd $(DIR); rm -rf config.cache; \
+		libtoolize -if && aclocal -I m4 && autoheader && \
+		automake --add-missing --copy --foreign && autoconf && \
+		./configure --prefix=/usr --host=$(GNU_TARGET_NAME) );
 #	cat defs.patch|patch -p1 -d $(DIR)
 	make $(MAKEOPTS) -C $(DIR)
 	make $(MAKEOPTS) DESTDIR=$(TARGET_DIR) -C $(DIR) install
-	-$(BT_STRIP) $(BT_STRIP_BINOPTS) $(TARGET_DIR)/bin/*
-	-$(BT_STRIP) $(BT_STRIP_LIBOPTS) $(TARGET_DIR)/lib/*
-	-rm -rf $(TARGET_DIR)/share
-	-rm -rf $(TARGET_DIR)/man
+	exit 1
+	-$(BT_STRIP) $(BT_STRIP_BINOPTS) $(TARGET_DIR)/usr/bin/*
+	-$(BT_STRIP) $(BT_STRIP_LIBOPTS) $(TARGET_DIR)/usr/lib/*
+	-rm -rf $(TARGET_DIR)/usr/share
+	-rm -rf $(TARGET_DIR)/usr/man
 	cp -R $(TARGET_DIR)/* $(BT_STAGING_DIR)
 	touch $(DIR)/.build
 
