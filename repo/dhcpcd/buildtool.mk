9913a595a9fa432a62acddacfcb52ba81e2a8838
Andrew Denisenko <nitr0man@users.sourceforge.net>
2011-11-02 20:52:24 +0300

    ported openssl, dhcpcd

--- repo/dhcpcd/buildtool.mk
+++ repo/dhcpcd/buildtool.mk
@@ -13,7 +13,7 @@ endif
 DHCPCD_TARGET_DIR:=$(BT_BUILD_DIR)/dhcpcd
 
 # Option settings for 'configure':
-CONFOPTS:= --build=$(GNU_TARGET_NAME) --host=$(GNU_HOST_NAME)
+CONFOPTS:= --host=$(GNU_TARGET_NAME) --prefix=/
 
 $(DHCPCD_DIR)/.source:
 	bzcat $(DHCPCD_SOURCE) | tar -xvf -
@@ -22,7 +22,7 @@ $(DHCPCD_DIR)/.source:
 	touch $(DHCPCD_DIR)/.source
 
 source: $(DHCPCD_DIR)/.source
-                        
+
 $(DHCPCD_DIR)/.configure: $(DHCPCD_DIR)/.source
 	( cd $(DHCPCD_DIR) ; ./configure $(CONFOPTS) )
 	touch $(DHCPCD_DIR)/.configure
@@ -35,24 +35,22 @@ $(DHCPCD_DIR)/.build: $(DHCPCD_DIR)/.configure
 	make -C $(DHCPCD_DIR) CC=$(TARGET_CC)
 	make -C $(DHCPCD_DIR) CC=$(TARGET_CC) DESTDIR=$(DHCPCD_TARGET_DIR) BINMODE=0755 NONBINMODE=0644 install
 	-$(BT_STRIP) $(BT_STRIP_BINOPTS) $(DHCPCD_TARGET_DIR)/sbin/dhcpcd
-	
-	cp -a $(DHCPCD_TARGET_DIR)/sbin/dhcpcd $(BT_STAGING_DIR)/sbin/dhcpcd
-	cp -a $(DHCPCD_TARGET_DIR)/libexec/dhcpcd-run-hooks $(BT_STAGING_DIR)/libexec/
-	cp -a $(DHCPCD_TARGET_DIR)/libexec/dhcpcd-hooks/* $(BT_STAGING_DIR)/libexec/dhcpcd-hooks/
-	
-	cp -aL dhcpcd.conf $(BT_STAGING_DIR)/etc/
-	cp -aL resolv.conf.head $(BT_STAGING_DIR)/etc/
+
+	rm -rf $(DHCPCD_TARGET_DIR)/share
+	cp -aL dhcpcd.conf $(DHCPCD_TARGET_DIR)/etc/
+	cp -aL resolv.conf.head $(DHCPCD_TARGET_DIR)/etc/
 	cp -aL resolv.conf.tail $(BT_STAGING_DIR)/etc/
+	cp -a $(DHCPCD_TARGET_DIR)/* $(BT_STAGING_DIR)/
 	touch $(DHCPCD_DIR)/.build
 
 build: $(DHCPCD_DIR)/.build
-                                                                                         
-clean: 
-	rm -rf $(DHCPCD_TARGET_DIR)
-	rm -f $(DHCPCD_DIR)/.build
-	rm -f $(DHCPCD_DIR)/.configure
-                                                                                                                 
+
+clean:
+	-rm -rf $(DHCPCD_TARGET_DIR)
+	-rm -f $(DHCPCD_DIR)/.build
+	-rm -f $(DHCPCD_DIR)/.configure
+
 srcclean: clean
-	rm -rf $(DHCPCD_DIR) 
-	rm DIRNAME
+	-rm -rf $(DHCPCD_DIR) 
+	-rm DIRNAME
 
--- repo/openssl/buildtool.mk
+++ repo/openssl/buildtool.mk
@@ -9,30 +9,30 @@ include $(MASTERMAKEFILE)
 OPENSSL_DIR:=openssl-1.0.0e
 OPENSSL_TARGET_DIR:=$(BT_BUILD_DIR)/openssl
 
-PERLVER=$(shell ls $(BT_STAGING_DIR)/usr/lib/perl5) 
-
 $(OPENSSL_DIR)/.source: 
 	zcat $(OPENSSL_SOURCE) | tar -xvf -
 #	zcat $(OPENSSL_PATCH1) | patch -d $(OPENSSL_DIR) -p1
 
-	# Clean up the configure script
-	perl -i -p -e 's,tcc=\"cc\";,tcc=\"$(TARGET_CC)\";,' $(OPENSSL_DIR)/Configure 
+#	# Clean up the configure script
+#	perl -i -p -e 's,tcc=\"cc\";,tcc=\"$(TARGET_CC)\";,' $(OPENSSL_DIR)/Configure 
 	touch $(OPENSSL_DIR)/.source
 
 $(OPENSSL_DIR)/.configured: $(OPENSSL_DIR)/.source
 	
-	([ -$(PERLVER) = - ] || export PERLLIB=$(BT_STAGING_DIR)/usr/lib/perl5/$(PERLVER); cd $(OPENSSL_DIR); \
+	(cd $(OPENSSL_DIR); \
 	./Configure linux-elf  \
 		--prefix=/usr \
 		--openssldir=/usr/ssl \
+		--install_prefix=$(OPENSSL_TARGET_DIR) \
+		--cross-compile-prefix=$(CROSS_COMPILE) \
+		no-ssl3 no-tls1 no-idea no-mdc2 no-rc5 no-hw no-krb5 shared no-fips no-threads  \
 		-L$(BT_STAGING_DIR)/lib -L$(BT_STAGING_DIR)/usr/lib \
 		-I$(BT_STAGING_DIR)/include -I$(BT_STAGING_DIR)/usr/include \
-		no-ssl3 no-tls1 no-idea no-mdc2 no-rc5 no-hw no-krb5 shared no-fips no-threads  \
-		 );	
-	perl -i -p -e 's,\s*gcc\s+,\t$(TARGET_CC) ,' $(OPENSSL_DIR)/util/domd 
-	perl -i -p -e 's,\s*gcc\s+,\t$(TARGET_CC) ,' $(OPENSSL_DIR)/util/pl/linux.pl 
-	perl -i -p -e 's,\s*gcc\s+,\t$(TARGET_CC) ,' $(OPENSSL_DIR)/crypto/bn/Makefile 	
-	perl -i -p -e 's,AR=ar.*,AR=$(BT_STAGING_DIR)/bin/$(GNU_TARGET_NAME)-ar r,' $(OPENSSL_DIR)/Makefile
+		 );
+#	perl -i -p -e 's,\s*gcc\s+,\t$(TARGET_CC) ,' $(OPENSSL_DIR)/util/domd 
+#	perl -i -p -e 's,\s*gcc\s+,\t$(TARGET_CC) ,' $(OPENSSL_DIR)/util/pl/linux.pl 
+#	perl -i -p -e 's,\s*gcc\s+,\t$(TARGET_CC) ,' $(OPENSSL_DIR)/crypto/bn/Makefile 	
+#	perl -i -p -e 's,AR=ar.*,AR=$(BT_STAGING_DIR)/bin/$(GNU_TARGET_NAME)-ar r,' $(OPENSSL_DIR)/Makefile
 	touch $(OPENSSL_DIR)/.configured
 
 $(OPENSSL_DIR)/.build: $(OPENSSL_DIR)/.configured
@@ -43,21 +43,16 @@ $(OPENSSL_DIR)/.build: $(OPENSSL_DIR)/.configured
 	-mkdir -p $(BT_STAGING_DIR)/etc/ssl
 	-mkdir -p $(BT_STAGING_DIR)/etc/certs
 	-mkdir -p $(BT_STAGING_DIR)/etc/private
-	-mkdir -p $(BT_STAGING_DIR)/usr/ssl/misc	
-	
-	export PERLLIB=$(BT_STAGING_DIR)/usr/lib/perl5/$(PERLVER); \
-	make CC=$(TARGET_CC) -C $(OPENSSL_DIR) depend
-	make CC=$(TARGET_CC) -C $(OPENSSL_DIR) 
-	$(BT_STRIP) --strip-unneeded $(OPENSSL_DIR)/libcrypto.so.1.0.0
-	$(BT_STRIP) --strip-unneeded $(OPENSSL_DIR)/libssl.so.1.0.0		
+	-mkdir -p $(BT_STAGING_DIR)/usr/ssl/misc
+
+	make -C $(OPENSSL_DIR) depend
+	make -C $(OPENSSL_DIR)
 	make CC=$(TARGET_CC) -C $(OPENSSL_DIR) INSTALL_PREFIX=$(OPENSSL_TARGET_DIR) install
+	-$(BT_STRIP) $(BT_STRIP_LIBOPTS) $(OPENSSL_TARGET_DIR)/usr/lib/* $(OPENSSL_TARGET_DIR)/usr/lib/engines/*
+	-$(BT_STRIP) $(BT_STRIP_BINOPTS) $(OPENSSL_TARGET_DIR)/usr/bin/*
 	$(BT_STRIP) -s --remove-section=.note --remove-section=.comment $(OPENSSL_TARGET_DIR)/usr/bin/openssl
-	cp -af $(OPENSSL_TARGET_DIR)/usr/bin/openssl  $(BT_STAGING_DIR)/usr/bin/
-	cp -af $(OPENSSL_TARGET_DIR)/usr/bin/c_rehash  $(BT_STAGING_DIR)/usr/bin/
-	cp -af $(OPENSSL_TARGET_DIR)/usr/lib/lib*  $(BT_STAGING_DIR)/usr/lib/
-	cp -af $(OPENSSL_TARGET_DIR)/usr/include/* $(BT_STAGING_DIR)/usr/include/  	
-	cp -af $(OPENSSL_TARGET_DIR)/usr/ssl/misc/* $(BT_STAGING_DIR)/usr/ssl/misc/  	
-	cp -f $(OPENSSL_TARGET_DIR)/usr/ssl/openssl.cnf $(BT_STAGING_DIR)/etc/ssl/
+	rm -rf $(OPENSSL_TARGET_DIR)/usr/ssl/man
+	cp -a $(OPENSSL_TARGET_DIR)/* $(BT_STAGING_DIR)
 	# workaround for strange behaviour of install, dir is not readable and so not removable
 	-chmod 755 $(OPENSSL_TARGET_DIR)/usr/lib/pkgconfig
 	touch $(OPENSSL_DIR)/.build
@@ -68,15 +63,17 @@ build: $(OPENSSL_DIR)/.build
 
 clean: 
 	-rm $(OPENSSL_DIR)/.build
-	rm -f $(BT_STAGING_DIR)/bin/openssl  
-	rm -f $(BT_STAGING_DIR)/lib/libcrypto.so* 
-	rm -f $(BT_STAGING_DIR)/lib/libssl.so* 
-	rm -rf $(OPENSSL_BUILD_DIR)
+	-rm -f $(BT_STAGING_DIR)/bin/openssl  
+	-rm -f $(BT_STAGING_DIR)/usr/lib/libcrypto.so* 
+	-rm -f $(BT_STAGING_DIR)/usr/lib/libssl.so* 
+	-rm -rf $(BT_STAGING_DIR)/usr/ssl
+	-rm -rf $(OPENSSL_BUILD_DIR)
 	-$(MAKE) -C $(OPENSSL_DIR) clean
 
 srcclean: 
-	rm -f $(BT_STAGING_DIR)/bin/openssl  
-	rm -f $(BT_STAGING_DIR)/lib/libcrypto.so* 
-	rm -f $(BT_STAGING_DIR)/lib/libssl.so* 
-	rm -rf $(OPENSSL_BUILD_DIR)
-	rm -rf $(OPENSSL_DIR)
+	-rm -f $(BT_STAGING_DIR)/bin/openssl  
+	-rm -f $(BT_STAGING_DIR)/usr/lib/libcrypto.so* 
+	-rm -f $(BT_STAGING_DIR)/usr/lib/libssl.so* 
+	-rm -rf $(BT_STAGING_DIR)/usr/ssl
+	-rm -rf $(OPENSSL_BUILD_DIR)
+	-rm -rf $(OPENSSL_DIR)
