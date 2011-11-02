b5a27f3678f9258ea66eb3da562b64014a16cde2
Andrew Denisenko <nitr0man@users.sourceforge.net>
2011-11-02 19:46:49 +0300

    fixed packages; updated perl+added cross-compilation environment

--- repo/dnsmasq/buildtool.mk
+++ repo/dnsmasq/buildtool.mk
@@ -9,18 +9,17 @@ $(DNSMASQ_DIR)/.source:
 	touch $(DNSMASQ_DIR)/.source
 
 source: $(DNSMASQ_DIR)/.source
-                        
+
 $(DNSMASQ_DIR)/.configured: $(DNSMASQ_DIR)/.source
 	touch $(DNSMASQ_DIR)/.configured
-                                                                 
+
 $(DNSMASQ_DIR)/.build: $(DNSMASQ_DIR)/.configured
 	mkdir -p $(DNSMASQ_TARGET_DIR)
-	mkdir -p $(DNSMASQ_TARGET_DIR)/usr/sbin	
-	mkdir -p $(DNSMASQ_TARGET_DIR)/etc/init.d	
+	mkdir -p $(DNSMASQ_TARGET_DIR)/usr/sbin
+	mkdir -p $(DNSMASQ_TARGET_DIR)/etc/init.d
 	# Disable the internal tftpd with -DNO_TFTP
 	make -C $(DNSMASQ_DIR) DESTDIR=$(DNSMASQ_TARGET_DIR) \
-	CC=$(TARGET_CC) LD=$(TARGET_LD) CFLAGS="$(BT_COPT_FLAGS) -DNO_TFTP" all  
-	-$(BT_STRIP) -s --remove-section=.note --remove-section=.comment $(DNSMASQ_DIR)/src/dnsmasq	
+	CC=$(TARGET_CC) LD=$(TARGET_LD) CFLAGS="$(CFLAGS) -DNO_TFTP" all
 	# Adjust the dnsmasq.conf to LEAF standard
 	perl -i -p -e 's,\#dhcp-range=192.168.0.50\,192.168.0.150\,12h,\#dhcp-range=192.168.1.1\,192.168.1.199\,12h,g' $(DNSMASQ_DIR)/dnsmasq.conf.example
 	perl -i -p -e 's,\#domain=thekelleys.org.uk,domain=private.network,g' $(DNSMASQ_DIR)/dnsmasq.conf.example
@@ -28,16 +27,17 @@ $(DNSMASQ_DIR)/.build: $(DNSMASQ_DIR)/.configured
 	cp -aL dnsmasq.init $(DNSMASQ_TARGET_DIR)/etc/init.d/dnsmasq
 	cp -a $(DNSMASQ_DIR)/src/dnsmasq $(DNSMASQ_TARGET_DIR)/usr/sbin
 	cp -a $(DNSMASQ_DIR)/dnsmasq.conf.example $(DNSMASQ_TARGET_DIR)/etc/dnsmasq.conf
+	-$(BT_STRIP) -s --remove-section=.note --remove-section=.comment $(DNSMASQ_TARGET_DIR)/usr/sbin/dnsmasq
 	cp -a $(DNSMASQ_TARGET_DIR)/* $(BT_STAGING_DIR)
 	touch $(DNSMASQ_DIR)/.build
 
 build: $(DNSMASQ_DIR)/.build
-                                                                                         
+
 clean:
 	make -C $(DNSMASQ_DIR) clean
 	rm -rf $(DNSMASQ_TARGET_DIR)
 	rm -rf $(DNSMASQ_DIR)/.build
 	rm -rf $(DNSMASQ_DIR)/.configured
-                                                                                                                 
+
 srcclean: clean
 	rm -rf $(DNSMASQ_DIR) 
