03501ff897c6e5f1d04268717c99449c65160161
Andrew Denisenko <nitr0man@users.sourceforge.net>
2011-11-15 19:40:41 +0200

    updated dhcp-helper, modified vlan (now it contains only scripts, vconfig us used from bb)

--- repo/dhcp-helper/buildtool.mk
+++ repo/dhcp-helper/buildtool.mk
@@ -10,7 +10,7 @@ DHCP-HELPER_DIR:=dhcp-helper-0.7
 DHCP-HELPER_TARGET_DIR:=$(BT_BUILD_DIR)/dhcp-helper
 
 
-$(DHCP-HELPER_DIR)/.source: 
+$(DHCP-HELPER_DIR)/.source:
 	zcat $(DHCP-HELPER_SOURCE) |  tar -xvf - 
 	touch $(DHCP-HELPER_DIR)/.source
 
@@ -19,7 +19,7 @@ $(DHCP-HELPER_DIR)/.build:
 	mkdir -p $(DHCP-HELPER_TARGET_DIR)/usr/sbin
 	mkdir -p $(DHCP-HELPER_TARGET_DIR)/etc/default
 	mkdir -p $(DHCP-HELPER_TARGET_DIR)/etc/init.d
-	$(MAKE) CC=$(TARGET_CC) -C $(DHCP-HELPER_DIR) PREFIX=/usr CFLAGS="$(BT_COPT_FLAGS)"
+	$(MAKE) CC=$(TARGET_CC) LD=$(TARGET_LD) -C $(DHCP-HELPER_DIR)
 	-$(BT_STRIP) $(BT_STRIP_BINOPTS) $(DHCP-HELPER_DIR)/dhcp-helper
 	cp -a $(DHCP-HELPER_DIR)/dhcp-helper $(DHCP-HELPER_TARGET_DIR)/usr/sbin
 	cp -aL dhcp-helper.init $(DHCP-HELPER_TARGET_DIR)/etc/init.d/dhcp-helper
@@ -34,6 +34,6 @@ build: $(DHCP-HELPER_DIR)/.build
 clean:
 	-rm $(DHCP-HELPER_DIR)/.build
 	$(MAKE) -C $(DHCP-HELPER_DIR) clean
-  
+
 srcclean:
 	rm -rf $(DHCP-HELPER_DIR)
