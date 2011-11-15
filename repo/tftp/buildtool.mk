b9cf2240ab5c867c8cab85201460eb6f7586561d
Andrew Denisenko <nitr0man@users.sourceforge.net>
2011-11-15 19:33:43 +0200

    updated tftpd

--- repo/tftp/buildtool.mk
+++ repo/tftp/buildtool.mk
@@ -4,39 +4,36 @@ include $(MASTERMAKEFILE)
 TFTP_DIR:=tftp-hpa-0.41
 TFTP_TARGET_DIR:=$(BT_BUILD_DIR)/tftp
 
-TFTP_LIBS = -liberty
-TFTPD_LIBS = -lwrap -liberty
-
 $(TFTP_DIR)/.source:
 	zcat $(TFTP_SOURCE) | tar -xvf -
 	touch $(TFTP_DIR)/.source
 
 source: $(TFTP_DIR)/.source
-                        
+
 $(TFTP_DIR)/.configured: $(TFTP_DIR)/.source
-	(cd $(TFTP_DIR) ; CC=$(TARGET_CC) LD=$(TARGET_LD) CFLAGS="$(BT_COPT_FLAGS)" ./configure --prefix=/usr --without-readline )
+	(cd $(TFTP_DIR) ; \
+		./configure --prefix=/usr --without-readline --host=$(GNU_TARGET_NAME) )
 	touch $(TFTP_DIR)/.configured
-                                                                 
+
 $(TFTP_DIR)/.build: $(TFTP_DIR)/.configured
 	mkdir -p $(TFTP_TARGET_DIR)
-	mkdir -p $(TFTP_TARGET_DIR)/usr/sbin	
-	mkdir -p $(TFTP_TARGET_DIR)/usr/bin		
-	make -C $(TFTP_DIR) TFTP_LIBS="$(TFTP_LIBS)" TFTPD_LIBS="$(TFTPD_LIBS)"
-	-$(BT_STRIP) -s --remove-section=.note --remove-section=.comment $(TFTP_DIR)/tftpd/tftpd
-	-$(BT_STRIP) -s --remove-section=.note --remove-section=.comment $(TFTP_DIR)/tftp/tftp
+	mkdir -p $(TFTP_TARGET_DIR)/usr/sbin
+	mkdir -p $(TFTP_TARGET_DIR)/usr/bin
+	make $(MAKEOPTS) -C $(TFTP_DIR)
+	-$(BT_STRIP) $(BT_STRIP_BINOPTS) $(TFTP_DIR)/tftpd/tftpd
+	-$(BT_STRIP) $(BT_STRIP_BINOPTS) $(TFTP_DIR)/tftp/tftp
 	cp -a $(TFTP_DIR)/tftpd/tftpd $(TFTP_TARGET_DIR)/usr/sbin/in.tftpd
 	cp -a $(TFTP_DIR)/tftp/tftp $(TFTP_TARGET_DIR)/usr/bin
 	cp -a $(TFTP_TARGET_DIR)/* $(BT_STAGING_DIR)
 	touch $(TFTP_DIR)/.build
 
 build: $(TFTP_DIR)/.build
-                                                                                         
+
 clean:
 	make -C $(TFTP_DIR) clean
 	rm -rf $(TFTP_TARGET_DIR)
-	rm -rf $(TFTP_DIR)/.build
-	rm -rf $(TFTP_DIR)/.configured
-                                                                                                                 
+	rm -f $(TFTP_DIR)/.build
+	rm -f $(TFTP_DIR)/.configured
+
 srcclean: clean
-	rm -rf $(TFTP_DIR) 
-	rm -rf $(TFTP_DIR)/.source
+	rm -rf $(TFTP_DIR)
