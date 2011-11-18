a65106bb2b1361c48ba9f5b9a1279772b1625b81
Andrew Denisenko <nitr0man@users.sourceforge.net>
2011-11-18 23:33:14 +0200

    updated openrrcp

--- repo/openrrcp/buildtool.mk
+++ repo/openrrcp/buildtool.mk
@@ -10,8 +10,11 @@ OPENRRCP_DIR:=openrrcp-0.2.1
 OPENRRCP_TARGET_DIR:=$(BT_BUILD_DIR)/openrrcp
 
 
-source: 
+source: $(OPENRRCP_DIR)/Makefile
+
+$(OPENRRCP_DIR)/Makefile:
 	zcat $(SOURCE) |  tar -xvf -
+	cat $(PATCH1) | patch -p1 -d $(OPENRRCP_DIR)
 
 build: $(OPENRRCP_DIR)/Makefile
 	mkdir -p $(OPENRRCP_TARGET_DIR)
@@ -20,9 +23,10 @@ build: $(OPENRRCP_DIR)/Makefile
 	$(MAKE) KERNEL_INCLUDE=$(BT_LINUX_DIR)-$(BT_KERNEL_RELEASE)/include  \
 		LIBC_INCLUDE=$(BT_STAGING_DIR)/include \
 		CC=$(TARGET_CC) \
-		CCOPTS="-D_GNU_SOURCE $(BT_COPT_FLAGS) -Wstrict-prototypes -Wall " \
-		-C $(OPENRRCP_DIR) 
-		
+		CCOPTS="-D_GNU_SOURCE $(CFLAGS) -Wstrict-prototypes -Wall " \
+		$(MAKEOPTS) \
+		-C $(OPENRRCP_DIR)
+
 	-$(BT_STRIP) $(BT_STRIP_BINOPTS) $(OPENRRCP_DIR)/bin/rrcpcli
 	-$(BT_STRIP) $(BT_STRIP_BINOPTS) $(OPENRRCP_DIR)/bin/rtl83xx
 	cp -a  $(OPENRRCP_DIR)/bin/* $(OPENRRCP_TARGET_DIR)/usr/bin
@@ -30,6 +34,6 @@ build: $(OPENRRCP_DIR)/Makefile
 
 clean:
 	-rm -f $(OPENRRCP_DIR)/bin/*
- 
+
 srcclean:
 	rm -rf $(OPENRRCP_DIR)
