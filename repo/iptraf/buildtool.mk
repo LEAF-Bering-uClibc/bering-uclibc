83a5a14eb71a556804ea2bf635fc1d52f1ee82b5
Andrew Denisenko <nitr0man@users.sourceforge.net>
2011-11-19 00:15:00 +0200

    updated iptraf

--- repo/iptraf/buildtool.mk
+++ repo/iptraf/buildtool.mk
@@ -3,29 +3,35 @@ include $(MASTERMAKEFILE)
 
 IPTRAF_DIR:=iptraf-2.7.0
 IPTRAF_TARGET_DIR:=$(BT_BUILD_DIR)/iptraf
+ENVVAR=WORKDIR=/var/lib/iptraf \
+	LOGDIR=/var/log/iptraf \
+	TARGET=/usr/sbin \
+	CC=$(TARGET_CC) \
+	AR=$(CROSS_COMPILE)ar \
+	RANLIB=$(CROSS_COMPILE)ranlib \
+	CFLAGS="$(CFLAGS)" LDOPTS="$(LDFLAGS)"
+
 
 $(IPTRAF_DIR)/.source:
 	zcat $(IPTRAF_SOURCE) | tar -xvf -
 	zcat $(IPTRAF_PATCH1) | patch -d $(IPTRAF_DIR) -p1
 	zcat $(IPTRAF_PATCH2) | patch -d $(IPTRAF_DIR) -p1
+	cat $(IPTRAF_PATCH3) | patch -d $(IPTRAF_DIR)/support -p0
 	touch $(IPTRAF_DIR)/.source
 
 source: $(IPTRAF_DIR)/.source
-                        
+
 $(IPTRAF_DIR)/.configured: $(IPTRAF_DIR)/.source
 	perl -i -p -e 's,INCLUDEDIR\s*=\s*-I/usr/include/ncurses -I../support,INCLUDEDIR = -I$(BT_STAGING_DIR)/usr/include -I../support,ig' $(IPTRAF_DIR)/src/Makefile
 	perl -i -p -e 's,INCLUDEDIR\s*=\s*-I/usr/include/ncurses,INCLUDEDIR = -I$(BT_STAGING_DIR)/usr/include,ig' $(IPTRAF_DIR)/support/Makefile
 	touch $(IPTRAF_DIR)/.configured
-                                                                 
+
 $(IPTRAF_DIR)/.build: $(IPTRAF_DIR)/.configured
 	mkdir -p $(IPTRAF_TARGET_DIR)
 	mkdir -p $(IPTRAF_TARGET_DIR)/var/log/iptraf
-	mkdir -p $(IPTRAF_TARGET_DIR)/var/run/iptraf		
-	mkdir -p $(IPTRAF_TARGET_DIR)/usr/sbin	
-	make -C $(IPTRAF_DIR)/src \
-		WORKDIR=/var/lib/iptraf \
-		LOGDIR=/var/log/iptraf \
-		TARGET=/usr/sbin 
+	mkdir -p $(IPTRAF_TARGET_DIR)/var/run/iptraf
+	mkdir -p $(IPTRAF_TARGET_DIR)/usr/sbin
+	make $(MAKEOPTS) -C $(IPTRAF_DIR)/src $(ENVVAR)
 	-$(BT_STRIP) $(BT_STRIP_BINOPTS) $(IPTRAF_DIR)/src/iptraf
 	-$(BT_STRIP) $(BT_STRIP_BINOPTS) $(IPTRAF_DIR)/src/rvnamed
 	cp -a $(IPTRAF_DIR)/src/iptraf $(IPTRAF_TARGET_DIR)/usr/sbin
@@ -34,14 +40,13 @@ $(IPTRAF_DIR)/.build: $(IPTRAF_DIR)/.configured
 	touch $(IPTRAF_DIR)/.build
 
 build: $(IPTRAF_DIR)/.build
-                                                                                         
+
 clean:
 	make -C $(IPTRAF_DIR)/src clean
 	make -C $(IPTRAF_DIR)/support clean
 	rm -rf $(IPTRAF_TARGET_DIR)
 	rm -rf $(IPTRAF_DIR)/.build
 	rm -rf $(IPTRAF_DIR)/.configured
-                                                                                                                 
+
 srcclean: clean
 	rm -rf $(IPTRAF_DIR) 
-	rm -rf $(IPTRAF_DIR)/.source
--- repo/ncurses/buildtool.mk
+++ repo/ncurses/buildtool.mk
@@ -44,7 +44,6 @@ $(NCURSES_DIR)/.configured: $(NCURSES_DIR)/.source
 $(NCURSES_DIR)/.build: $(NCURSES_DIR)/.configured
 	mkdir -p $(NCURSES_BUILD_DIR)
 	make $(MAKEOPTS) -C $(NCURSES_DIR)
-	exit 1
 	make -C $(NCURSES_DIR) install
 	-$(BT_STRIP) $(BT_STRIP_LIBOPTS) $(NCURSES_BUILD_DIR)/usr/lib/*
 	cp -a $(NCURSES_BUILD_DIR)/* $(BT_STAGING_DIR)
