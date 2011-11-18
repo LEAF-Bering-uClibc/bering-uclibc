f7480ef86123c7fdefe49c1bbd8237ba9cd216d0
Andrew Denisenko <nitr0man@users.sourceforge.net>
2011-11-18 22:54:36 +0200

    updated bridge package

--- repo/bridge/buildtool.mk
+++ repo/bridge/buildtool.mk
@@ -3,8 +3,6 @@ include $(MASTERMAKEFILE)
 
 BRIDGE-UTILS_DIR:=bridge-utils-1.2
 BRIDGE-UTILS_TARGET_DIR:=$(BT_BUILD_DIR)/bridge
-export AUTOCONF=$(BT_STAGING_DIR)/bin/autoconf
-PERLVER=$(shell ls $(BT_STAGING_DIR)/usr/lib/perl5 2>/dev/null)
 
 $(BRIDGE-UTILS_DIR)/.source:
 	zcat $(BRIDGE-UTILS_SOURCE) | tar -xvf -
@@ -12,35 +10,35 @@ $(BRIDGE-UTILS_DIR)/.source:
 	touch $(BRIDGE-UTILS_DIR)/.source
 
 source: $(BRIDGE-UTILS_DIR)/.source
-                        
+
 $(BRIDGE-UTILS_DIR)/.configured: $(BRIDGE-UTILS_DIR)/.source
-	(cd $(BRIDGE-UTILS_DIR) ;  export PERLLIB=$(BT_STAGING_DIR)/usr/lib/perl5/$(PERLVER); \
-	$(AUTOCONF) ; CC=$(TARGET_CC) LD=$(TARGET_LD) CFLAGS="$(BT_COPT_FLAGS)" \
-	./configure --prefix=/usr --with-linux-headers=$(BT_LINUX_DIR)-$(BT_KERNEL_RELEASE)/include )
+	(cd $(BRIDGE-UTILS_DIR) ; autoreconf -i -f ;\
+	./configure --prefix=/usr --host=$(GNU_TARGET_NAME) \
+	--with-linux-headers=$(BT_LINUX_DIR)-$(BT_KERNEL_RELEASE)/include )
 	touch $(BRIDGE-UTILS_DIR)/.configured
-                                                                 
+
 $(BRIDGE-UTILS_DIR)/.build: $(BRIDGE-UTILS_DIR)/.configured
 	mkdir -p $(BRIDGE-UTILS_TARGET_DIR)
 	mkdir -p $(BRIDGE-UTILS_TARGET_DIR)/usr/sbin
 	mkdir -p $(BRIDGE-UTILS_TARGET_DIR)/etc/network/if-pre-up.d
 	mkdir -p $(BRIDGE-UTILS_TARGET_DIR)/etc/network/if-post-down.d
 	mkdir -p $(BRIDGE-UTILS_TARGET_DIR)/etc/network/if-up.d
-	make -C $(BRIDGE-UTILS_DIR) all  
+	make $(MAKEOPTS) -C $(BRIDGE-UTILS_DIR) all
 	-$(BT_STRIP) $(BT_STRIP_BINOPTS) $(BRIDGE-UTILS_DIR)/brctl/brctl
 	cp -a $(BRIDGE-UTILS_DIR)/brctl/brctl $(BRIDGE-UTILS_TARGET_DIR)/usr/sbin
-	cp -aL bridge_if-pre-up $(BRIDGE-UTILS_TARGET_DIR)/etc/network/if-pre-up.d/bridge	
+	cp -aL bridge_if-pre-up $(BRIDGE-UTILS_TARGET_DIR)/etc/network/if-pre-up.d/bridge
 	cp -aL bridge_if-post-down $(BRIDGE-UTILS_TARGET_DIR)/etc/network/if-post-down.d/bridge
 	cp -aL bridge_if-up $(BRIDGE-UTILS_TARGET_DIR)/etc/network/if-up.d/bridge
 	cp -a $(BRIDGE-UTILS_TARGET_DIR)/* $(BT_STAGING_DIR)
 	touch $(BRIDGE-UTILS_DIR)/.build
 
 build: $(BRIDGE-UTILS_DIR)/.build
-                                                                                         
+
 clean:
 	make -C $(BRIDGE-UTILS_DIR) clean
 	rm -rf $(BRIDGE-UTILS_TARGET_DIR)
 	rm -f $(BRIDGE-UTILS_DIR)/.build
 	rm -f $(BRIDGE-UTILS_DIR)/.configured
-                                                                                                                 
+
 srcclean: clean
-	rm -rf $(BRIDGE-UTILS_DIR) 
+	rm -rf $(BRIDGE-UTILS_DIR)
