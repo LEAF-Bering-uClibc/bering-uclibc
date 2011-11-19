0254a5b217700965f9c0278372f2e0ae69504e62
Andrew Denisenko <nitr0man@users.sourceforge.net>
2011-11-19 14:18:32 +0200

    updated mtools

--- repo/mtools/buildtool.mk
+++ repo/mtools/buildtool.mk
@@ -13,34 +13,31 @@ export CC=$(TARGET_CC)
 $(MTOOLS_DIR)/.source: 
 	zcat $(MTOOLS_SOURCE) |  tar -xvf - 
 	zcat $(MTOOLS_PATCH1) | patch -d $(MTOOLS_DIR) -p1
-	#perl -i -p -e 's,CC\s*=\s*gcc,#CC = gcc,' $(MTOOLS_DIR)/Makefile
-	#perl -i -p -e 's,PREFIX\s*=.*,PREFIX = $(MTOOLS_TARGET_DIR),' $(MTOOLS_DIR)/Makefile
-	-mkdir $(MTOOLS_TARGET_DIR)
-	touch $(MTOOLS_DIR)/.source	
+	touch $(MTOOLS_DIR)/.source
 
 $(MTOOLS_DIR)/.configured: $(MTOOLS_DIR)/.source
 
 	( cd $(MTOOLS_DIR) ; CFLAGS="$(BT_COPT_FLAGS) -Wall" ./configure \
 		--prefix=/usr \
 		--sysconfdir=/etc \
-		--target=i386-pc-linux-gnu \
-		--with-build-libs="$(BT_STAGING_DIR)/lib" \
+		--target=$(GNU_HOST_NAME) \
 		--disable-floppyd \
 		--disable-debug \
 		--disable-new-vold \
 		--disable-vold 	;)
-	touch $(MTOOLS_DIR)/.configured		
+	touch $(MTOOLS_DIR)/.configured
 
 $(MTOOLS_DIR)/.build: $(MTOOLS_DIR)/.configured
-	$(MAKE) CC=$(TARGET_CC) -C $(MTOOLS_DIR) 
-	-$(BT_STRIP) --strip-unneeded $(MTOOLS_DIR)/mtools
-	cp $(MTOOLS_DIR)/mtools $(MTOOLS_TARGET_DIR)/
-	cp $(MTOOLS_DIR)/debian/mtools.conf $(MTOOLS_TARGET_DIR)/
-	mkdir -p $(BT_STAGING_DIR)/usr/bin
-	cp $(MTOOLS_TARGET_DIR)/*  $(BT_STAGING_DIR)/usr/bin
+	mkdir -p $(MTOOLS_TARGET_DIR)/etc
+	$(MAKE) $(MAKEOPTS) -C $(MTOOLS_DIR)
+	$(MAKE) prefix=$(MTOOLS_TARGET_DIR)/usr -C $(MTOOLS_DIR) install
+	-$(BT_STRIP) $(BT_STRIP_BINOPTS) $(MTOOLS_TARGET_DIR)/usr/bin/*
+	-rm -rf $(MTOOLS_TARGET_DIR)/usr/info $(MTOOLS_TARGET_DIR)/usr/man
+	cp $(MTOOLS_DIR)/debian/mtools.conf $(MTOOLS_TARGET_DIR)/etc/
+	cp -a $(MTOOLS_TARGET_DIR)/*  $(BT_STAGING_DIR)/
 	touch $(MTOOLS_DIR)/.build
 
-source: $(MTOOLS_DIR)/.source 
+source: $(MTOOLS_DIR)/.source
 
 build: $(MTOOLS_DIR)/.build
 
