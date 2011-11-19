64f0c5b9c59ecb14f4edab446a889a66012252aa
Andrew Denisenko <nitr0man@users.sourceforge.net>
2011-11-19 18:52:50 +0200

    updated netstat-nat

--- repo/netstat-nat/buildtool.mk
+++ repo/netstat-nat/buildtool.mk
@@ -11,16 +11,18 @@ $(NETSTAT_NAT_DIR)/.source:
 source: $(NETSTAT_NAT_DIR)/.source
 
 $(NETSTAT_NAT_DIR)/.configured: $(NETSTAT_NAT_DIR)/.source
-	(cd $(NETSTAT_NAT_DIR) ; CC=$(TARGET_CC) LD=$(TARGET_LD) CFLAGS="$(BT_COPT_FLAGS)" \
-	./configure --prefix=/usr)
+	(cd $(NETSTAT_NAT_DIR) ; ./configure \
+		--prefix=/usr \
+		--host=$(GNU_TARGET_NAME) \
+	)
 	touch $(NETSTAT_NAT_DIR)/.configured
 
 $(NETSTAT_NAT_DIR)/.build: $(NETSTAT_NAT_DIR)/.configured
 	mkdir -p $(NETSTAT_NAT_TARGET_DIR)
 	mkdir -p $(NETSTAT_NAT_TARGET_DIR)/usr/bin
-	make CC="$(TARGET_CC)" -C $(NETSTAT_NAT_DIR)
-	-$(BT_STRIP) $(BT_STRIP_BINOPTS) $(NETSTAT_NAT_DIR)/netstat-nat
+	make $(MAKEOPTS) -C $(NETSTAT_NAT_DIR)
 	cp -a $(NETSTAT_NAT_DIR)/netstat-nat $(NETSTAT_NAT_TARGET_DIR)/usr/bin
+	-$(BT_STRIP) $(BT_STRIP_BINOPTS) $(NETSTAT_NAT_TARGET_DIR)/usr/bin/*
 	cp -a $(NETSTAT_NAT_TARGET_DIR)/* $(BT_STAGING_DIR)
 	touch $(NETSTAT_NAT_DIR)/.build
 
