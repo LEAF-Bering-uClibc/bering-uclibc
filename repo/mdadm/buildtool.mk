12a9ee976358827cdc27b3e04b8b89b4a03c2490
Andrew Denisenko <nitr0man@users.sourceforge.net>
2011-11-19 16:47:17 +0200

    updated mdadm

--- repo/mdadm/buildtool.mk
+++ repo/mdadm/buildtool.mk
@@ -11,14 +11,14 @@ $(MDADM_DIR)/.source:
 
 source: $(MDADM_DIR)/.source
 
-                        
 $(MDADM_DIR)/.build:
 	mkdir -p $(MDADM_TARGET_DIR)
 	mkdir -p $(MDADM_TARGET_DIR)/sbin
 	mkdir -p $(MDADM_TARGET_DIR)/etc/init.d
-	$(MAKE) CC=$(TARGET_CC) CXFLAGS="$(BT_COPT_FLAGS) -DUCLIBC -fno-strict-aliasing" -C $(MDADM_DIR)
-	$(BT_STRIP) $(BT_STRIP_BINOPTS) $(MDADM_DIR)/mdadm
+	$(MAKE) $(MAKEOPTS) CC=$(TARGET_CC) \
+	    CFLAGS="$(CFLAGS) -DUCLIBC -fno-strict-aliasing" -C $(MDADM_DIR)
 	cp -a $(MDADM_DIR)/mdadm $(MDADM_TARGET_DIR)/sbin
+	-$(BT_STRIP) $(BT_STRIP_BINOPTS) $(MDADM_TARGET_DIR)/sbin/*
 	cp -aL mdadm.init $(MDADM_TARGET_DIR)/etc/init.d/mdadm
 	cp -aL mdadm-raid.init $(MDADM_TARGET_DIR)/etc/init.d/mdadm-raid
 	cp -aL mdadm.conf $(MDADM_TARGET_DIR)/etc
@@ -27,12 +27,12 @@ $(MDADM_DIR)/.build:
 	touch $(MDADM_DIR)/.build
 
 build: $(MDADM_DIR)/.build
-                                                                                         
+
 clean:
 	make -C $(MDADM_DIR) clean
 	rm -rf $(MDADM_TARGET_DIR)
 	rm -f $(MDADM_DIR)/.build
-                                                                                                                 
+
 srcclean: clean
-	rm -rf $(MDADM_DIR) 
+	rm -rf $(MDADM_DIR)
 	rm -f $(MDADM_DIR)/.source
