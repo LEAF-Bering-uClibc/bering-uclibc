9dac9b14ba6b818ab2276c36b8a99f11b5d97f35
Andrew Denisenko <nitr0man@users.sourceforge.net>
2011-11-19 13:11:48 +0200

    updated bash

--- repo/bash/buildtool.mk
+++ repo/bash/buildtool.mk
@@ -1,7 +1,7 @@
 # makefile for bash
 include $(MASTERMAKEFILE)
 
-BASH_DIR:=bash-3.2.48
+BASH_DIR:=bash-4.2
 BASH_TARGET_DIR:=$(BT_BUILD_DIR)/bash
 
 $(BASH_DIR)/.source:
@@ -11,26 +11,33 @@ $(BASH_DIR)/.source:
 source: $(BASH_DIR)/.source
                         
 $(BASH_DIR)/.configured: $(BASH_DIR)/.source
-	(cd $(BASH_DIR) ; CC=$(TARGET_CC) LD=$(TARGET_LD) CFLAGS="-Os" ./configure --host=i386-pc-linux-gnu \
+	(cd $(BASH_DIR) ; ./configure \
+			--host=$(GNU_TARGET_NAME) \
 			--with-curses --disable-net-redirections --prefix=/ \
-			--infodir=/usr/share/info --mandir=/usr/share/man )
+			--infodir=/usr/share/info --mandir=/usr/share/man \
+			--enable-command-timing \
+			--enable-debugger \
+			--enable-history \
+			--enable-readline \
+			--disable-nls --disable-rpath )
 	touch $(BASH_DIR)/.configured
-                                                                 
+
 $(BASH_DIR)/.build: $(BASH_DIR)/.configured
 	mkdir -p $(BASH_TARGET_DIR)
 	mkdir -p $(BASH_TARGET_DIR)/bin
-	make -C $(BASH_DIR) all  
-	-$(BT_STRIP) -s --remove-section=.note --remove-section=.comment $(BASH_DIR)/bash	
+	make $(MAKEOPTS) -C $(BASH_DIR) all
 	cp -a $(BASH_DIR)/bash $(BASH_TARGET_DIR)/bin
+	-$(BT_STRIP) $(BT_STRIP_BINOPTS) $(BASH_TARGET_DIR)/bin/bash
 	cp -a $(BASH_TARGET_DIR)/* $(BT_STAGING_DIR)
 	touch $(BASH_DIR)/.build
 
 build: $(BASH_DIR)/.build
-                                                                                         
+
 clean:
 	make -C $(BASH_DIR) clean
+	rm $(BASH_DIR)/.build $(BASH_DIR)/.configured
 	rm -rf $(BASH_TARGET_DIR)
-                                                                                                                 
+
 srcclean: clean
-	rm -rf $(BASH_DIR) 
+	rm -rf $(BASH_DIR)
 
