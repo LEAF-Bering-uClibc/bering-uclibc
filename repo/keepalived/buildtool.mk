699c97a03ed0726d287d85994408426354ea9f0c
Andrew Denisenko <nitr0man@users.sourceforge.net>
2011-11-19 12:50:47 +0200

    updated keepalived

--- repo/keepalived/buildtool.mk
+++ repo/keepalived/buildtool.mk
@@ -21,34 +21,27 @@ $(KEEPALIVED_DIR)/.source:
 	touch $(KEEPALIVED_DIR)/.source
 
 $(KEEPALIVED_DIR)/.configured: $(KEEPALIVED_DIR)/.source
-	(cd $(KEEPALIVED_DIR); rm -rf config.cache; autoconf; \
-		CPPFLAGS="-I$(BT_STAGING_DIR)/include -I$(BT_STAGING_DIR)/usr/include" \
-		LDFLAGS="-L $(BT_STAGING_DIR)/lib -L $(BT_STAGING_DIR)/usr/lib" \
-		LD_RUN_PATH="$(BT_STAGING_DIR)/lib:$(BT_STAGING_DIR)/usr/lib" \
-		CFLAGS="$(BT_COPT_FLAGS)" \
-		CC=$(TARGET_CC) \
-		LD=$(TARGET_LD) \
+	(cd $(KEEPALIVED_DIR); rm -rf config.cache; autoreconf -i -f; \
 		./configure \
 		--prefix=/usr \
-		--build=$(GNU_HOST_NAME) \
-		--host=$(GNU_HOST_NAME) \
+		--host=$(GNU_TARGET_NAME) \
 		--with-kernel-dir=$(BT_LINUX_DIR)-$(BT_KERNEL_RELEASE) \
 		 );
 		touch $(KEEPALIVED_DIR)/.configured
 
 $(KEEPALIVED_DIR)/.build: $(KEEPALIVED_DIR)/.configured
-	make CC=$(TARGET_CC) -C $(KEEPALIVED_DIR) all 
-	-mkdir -p $(KEEPALIVED_TARGET_DIR)/etc/keepalived	
-	-mkdir -p $(KEEPALIVED_TARGET_DIR)/etc/init.d	
-	-mkdir -p $(KEEPALIVED_TARGET_DIR)/usr/bin	
+	make $(MAKEOPTS) -C $(KEEPALIVED_DIR) all
+	-mkdir -p $(KEEPALIVED_TARGET_DIR)/etc/keepalived
+	-mkdir -p $(KEEPALIVED_TARGET_DIR)/etc/init.d
+	-mkdir -p $(KEEPALIVED_TARGET_DIR)/usr/bin
 	-mkdir -p $(KEEPALIVED_TARGET_DIR)/usr/sbin
-	-$(BT_STRIP) $(BT_STRIP_BINOPTS) $(KEEPALIVED_DIR)/bin/genhash	
-	-$(BT_STRIP) $(BT_STRIP_BINOPTS) $(KEEPALIVED_DIR)/bin/keepalived	
-	cp -aL keepalived.init $(KEEPALIVED_TARGET_DIR)/etc/init.d/keepalived 
-	cp -aL keepalived.conf $(KEEPALIVED_TARGET_DIR)/etc/keepalived/keepalived.conf 
+	-$(BT_STRIP) $(BT_STRIP_BINOPTS) $(KEEPALIVED_DIR)/bin/genhash
+	-$(BT_STRIP) $(BT_STRIP_BINOPTS) $(KEEPALIVED_DIR)/bin/keepalived
+	cp -aL keepalived.init $(KEEPALIVED_TARGET_DIR)/etc/init.d/keepalived
+	cp -aL keepalived.conf $(KEEPALIVED_TARGET_DIR)/etc/keepalived/keepalived.conf
 	cp -f $(KEEPALIVED_DIR)/bin/genhash $(KEEPALIVED_TARGET_DIR)/usr/bin/
 	cp -f $(KEEPALIVED_DIR)/bin/keepalived $(KEEPALIVED_TARGET_DIR)/usr/sbin/
-	cp -a -f $(KEEPALIVED_TARGET_DIR)/* $(BT_STAGING_DIR)  	
+	cp -a -f $(KEEPALIVED_TARGET_DIR)/* $(BT_STAGING_DIR)
 	touch $(KEEPALIVED_DIR)/.build
 
 source: $(KEEPALIVED_DIR)/.source
