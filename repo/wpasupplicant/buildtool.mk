dfdb05ab1abdcb8d50bfdb5d25a5a51e76c7f68f
Andrew Denisenko <nitr0man@users.sourceforge.net>
2011-11-19 13:31:41 +0200

    updated wpasupplicant

--- repo/wpasupplicant/buildtool.mk
+++ repo/wpasupplicant/buildtool.mk
@@ -11,36 +11,32 @@ TARGET_DIR:=$(BT_BUILD_DIR)/wpasupplicant
 
 source: .source
 
-                        
-.build: 
+.build:
 	mkdir -p $(TARGET_DIR)
 	mkdir -p $(TARGET_DIR)/usr/sbin
 	mkdir -p $(TARGET_DIR)/etc
 	mkdir -p $(TARGET_DIR)/etc/default
 	mkdir -p $(TARGET_DIR)/etc/init.d
 	cp -aL .config $(SOURCE_DIR)/.config
-	( cd $(SOURCE_DIR) ; export CFLAGS="$(BT_COPT_FLAGS) -MMD -Wall -g -I$(BT_STAGING_DIR)/usr/include" ; \
-	export LIBS=-L$(BT_STAGING_DIR)/usr/lib ; make CC=$(TARGET_CC) ) ;
-	-$(BT_STRIP) $(BT_STRIP_BINOPTS) $(SOURCE_DIR)/wpa_cli
-	-$(BT_STRIP) $(BT_STRIP_BINOPTS) $(SOURCE_DIR)/wpa_passphrase
-	-$(BT_STRIP) $(BT_STRIP_BINOPTS) $(SOURCE_DIR)/wpa_supplicant
-	cp -a $(SOURCE_DIR)/wpa_cli $(TARGET_DIR)/usr/sbin	
-	cp -a $(SOURCE_DIR)/wpa_passphrase $(TARGET_DIR)/usr/sbin	
-	cp -a $(SOURCE_DIR)/wpa_supplicant $(TARGET_DIR)/usr/sbin	
-	cp -aL wpa_supplicant.conf $(TARGET_DIR)/etc	
+	make $(MAKEOPTS) CC=$(TARGET_CC) -C $(SOURCE_DIR)
+	cp -a $(SOURCE_DIR)/wpa_cli $(TARGET_DIR)/usr/sbin
+	cp -a $(SOURCE_DIR)/wpa_passphrase $(TARGET_DIR)/usr/sbin
+	cp -a $(SOURCE_DIR)/wpa_supplicant $(TARGET_DIR)/usr/sbin
+	-$(BT_STRIP) $(BT_STRIP_BINOPTS) $(SOURCE_DIR)/usr/sbin/*
+	cp -aL wpa_supplicant.conf $(TARGET_DIR)/etc
 	cp -aL wpasupplicant.default $(TARGET_DIR)/etc/default/wpasupplicant
 	cp -aL wpasupplicant.init $(TARGET_DIR)/etc/init.d/wpasupplicant
 	cp -a $(TARGET_DIR)/* $(BT_STAGING_DIR)
 	touch .build
 
 build: .build
-                                                                                         
+
 clean:
 	make -C $(SOURCE_DIR) clean
 	rm -rf $(TARGET_DIR)
 	rm -f .build
-                                                                                                                 
+
 srcclean: clean
-	rm -rf $(SOURCE_DIR) 
+	rm -rf $(SOURCE_DIR)
 	rm -f .source
 	rm -f .configured
