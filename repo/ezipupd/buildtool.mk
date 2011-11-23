ae7dbac6dd21da01426e1cf8071272775e6ebe9f
Andrew Denisenko <nitr0man@users.sourceforge.net>
2011-11-23 21:37:05 +0200

    updated ezipupd

--- repo/ezipupd/buildtool.mk
+++ repo/ezipupd/buildtool.mk
@@ -3,27 +3,26 @@ include $(MASTERMAKEFILE)
 
 EZ_IPUPDATE_DIR:=ez-ipupdate-3.0.11b8
 EZ_IPUPDATE_TARGET_DIR:=$(BT_BUILD_DIR)/ez-ipupdate
-PERLVER=$(shell ls $(BT_STAGING_DIR)/usr/lib/perl5)
 
 $(EZ_IPUPDATE_DIR)/.source:
 	zcat $(EZ_IPUPDATE_SOURCE) | tar -xvf -
 	zcat $(EZ_IPUPDATE_PATCH1) | patch -d $(EZ_IPUPDATE_DIR) -p1
-	perl -i -p -e 's,^DESTDIR\s*=.*,DESTDIR = $(EZ_IPUPDATE_TARGET_DIR),g' $(EZ_IPUPDATE_DIR)/Makefile.in
+	cat $(EZ_IPUPDATE_PATCH2) | patch -d $(EZ_IPUPDATE_DIR) -p1
 	touch $(EZ_IPUPDATE_DIR)/.source
 
 source: $(EZ_IPUPDATE_DIR)/.source
 
 $(EZ_IPUPDATE_DIR)/.configured: $(EZ_IPUPDATE_DIR)/.source
-	(cd $(EZ_IPUPDATE_DIR) ; export PERLLIB=$(BT_STAGING_DIR)/usr/lib/perl5/$(PERLVER); \
-	CC=$(TARGET_CC) LD=$(TARGET_LD) CFLAGS="$(BT_COPT_FLAGS)" ./configure --prefix=/usr/ )
+	(cd $(EZ_IPUPDATE_DIR) ; autoreconf -i -f && \
+	./configure --prefix=/usr/ \
+	--host=$(GNU_TARGET_NAME) )
 	touch $(EZ_IPUPDATE_DIR)/.configured
 
 $(EZ_IPUPDATE_DIR)/.build: $(EZ_IPUPDATE_DIR)/.configured
 	mkdir -p $(EZ_IPUPDATE_TARGET_DIR)
 	mkdir -p $(EZ_IPUPDATE_TARGET_DIR)/etc/init.d
-	PERLLIB=$(BT_STAGING_DIR)/usr/lib/perl5/$(PERLVER) \
-	make -C $(EZ_IPUPDATE_DIR) DESTDIR=$(EZ_IPUPDATE_TARGET_DIR) install
-	-$(BT_STRIP) -s --remove-section=.note --remove-section=.comment $(EZ_IPUPDATE_TARGET_DIR)/usr/bin/ez-ipupdate
+	make $(MAKEOPTS) -C $(EZ_IPUPDATE_DIR) DESTDIR=$(EZ_IPUPDATE_TARGET_DIR) install
+	-$(BT_STRIP) $(BT_STRIP_BINOPTS) $(EZ_IPUPDATE_TARGET_DIR)/usr/bin/ez-ipupdate
 	cp -aL ez-ipupd $(EZ_IPUPDATE_TARGET_DIR)/etc/init.d/
 	cp -aL ez-ipupd.conf $(EZ_IPUPDATE_TARGET_DIR)/etc/
 	cp -a $(EZ_IPUPDATE_TARGET_DIR)/* $(BT_STAGING_DIR)
