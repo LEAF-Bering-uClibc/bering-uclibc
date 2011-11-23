2cd19433549f56811d0d1c8c97d234476eb2550e
Andrew Denisenko <nitr0man@users.sourceforge.net>
2011-11-24 00:01:46 +0200

    updated elvis

--- repo/elvis/buildtool.mk
+++ repo/elvis/buildtool.mk
@@ -14,12 +14,11 @@ source: $(ELVIS-TINY_DIR)/.source
 $(ELVIS-TINY_DIR)/.build: $(ELVIS-TINY_DIR)/.source
 	mkdir -p $(ELVIS-TINY_TARGET_DIR)
 	mkdir -p $(ELVIS-TINY_TARGET_DIR)/bin
-	make CC="$(TARGET_CC) -fsigned-char" -C $(ELVIS-TINY_DIR)
-	$(TARGET_CC) $(BT_COPT_FLAGS) -s -o $(ELVIS-TINY_DIR)/debian/wrapper $(ELVIS-TINY_DIR)/debian/wrapper.c
-	-$(BT_STRIP) $(BT_STRIP_BINOPTS) $(ELVIS-TINY_DIR)/elvis
-	-$(BT_STRIP) $(BT_STRIP_BINOPTS) $(ELVIS-TINY_DIR)/debian/wrapper
+	make $(MAKEOPTS) CC=$(TARGET_CC) EXTRA_CFLAGS="$(CFLAGS) $(LDFLAGS) -fsigned-char" -C $(ELVIS-TINY_DIR)
+	$(TARGET_CC) $(CFLAGS) -s -o $(ELVIS-TINY_DIR)/debian/wrapper $(ELVIS-TINY_DIR)/debian/wrapper.c
 	cp -a $(ELVIS-TINY_DIR)/elvis $(ELVIS-TINY_TARGET_DIR)/bin/elvis-tiny
 	cp -a $(ELVIS-TINY_DIR)/debian/wrapper $(ELVIS-TINY_TARGET_DIR)/bin/vi
+	-$(BT_STRIP) $(BT_STRIP_BINOPTS) $(ELVIS-TINY_TARGET_DIR)/bin/*
 	cp -a $(ELVIS-TINY_TARGET_DIR)/* $(BT_STAGING_DIR)
 	touch $(ELVIS-TINY_DIR)/.build
 
@@ -35,4 +34,3 @@ clean:
 
 srcclean: clean
 	rm -rf $(ELVIS-TINY_DIR)
-	rm -f $(ELVIS-TINY_DIR)/.source
