9bc74797ecc4e681ef3e30132789c8d1e00110c6
Andrew Denisenko <nitr0man@users.sourceforge.net>
2011-11-24 00:37:59 +0200

    updated pcre and snort

--- repo/snort/buildtool.mk
+++ repo/snort/buildtool.mk
@@ -10,7 +10,7 @@ SNORT_DIR:=snort-2.4.5
 SNORT_TARGET_DIR:=$(BT_BUILD_DIR)/snort
 
 
-CONFFLAGS:= --prefix=/usr \
+CONFFLAGS:= --prefix=/usr --host=$(GNU_TARGET_NAME) \
 	--with-libpcap_includes=$(BT_STAGING_DIR)/usr/include/ \
 	--with-libpcap_libraries=$(BT_STAGING_DIR)/usr/lib/ \
 	--with-libpcre_includes=$(BT_STAGING_DIR)/usr/include/ \
@@ -22,6 +22,7 @@ $(SNORT_DIR)/.source:
 	zcat $(SNORT_SOURCE) | tar -xvf -
 	zcat $(SNORT_PATCH) | patch -d $(SNORT_DIR) -p1
 	cd $(SNORT_DIR); zcat ../$(SNORT_RULES) | tar -xvf -
+	zcat $(BT_TOOLS_DIR)/config.sub.gz >$(SNORT_DIR)/config.sub
 	touch $(SNORT_DIR)/.source
 
 source: $(SNORT_DIR)/.source
@@ -29,16 +30,22 @@ source: $(SNORT_DIR)/.source
 
 $(SNORT_DIR)/.build: $(SNORT_DIR)/.source
 	mkdir -p $(SNORT_TARGET_DIR)
-	mkdir -p $(SNORT_TARGET_DIR)/usr/bin/
-	(cd $(SNORT_DIR) ; CC=$(TARGET_CC) LD=$(TARGET_LD) ./configure $(CONFFLAGS) --without-mysql )
-	make -C $(SNORT_DIR) CC=$(TARGET_CC) LD=$(TARGET_LD) CFLAGS="$(BT_COPT_FLAGS)"
-	cp -a -f $(SNORT_DIR)/src/snort $(SNORT_TARGET_DIR)/usr/bin/
-	$(BT_STRIP) $(BT_STRIP_BINOPTS) $(SNORT_TARGET_DIR)/usr/bin/snort
-	mkdir -p $(SNORT_TARGET_DIR)/etc/
-	mkdir -p $(SNORT_TARGET_DIR)/etc/snort/
 	mkdir -p $(SNORT_TARGET_DIR)/etc/snort/rules/
 	mkdir -p $(SNORT_TARGET_DIR)/etc/init.d
 	mkdir -p $(SNORT_TARGET_DIR)/etc/default
+	mkdir -p $(SNORT_TARGET_DIR)/usr/bin/
+	(cd $(SNORT_DIR) ; ./configure $(CONFFLAGS) --without-mysql )
+	make $(MAKEOPTS) -C $(SNORT_DIR)
+	cp -a -f $(SNORT_DIR)/src/snort $(SNORT_TARGET_DIR)/usr/bin/
+	-$(BT_STRIP) $(BT_STRIP_BINOPTS) $(SNORT_TARGET_DIR)/usr/bin/snort
+
+	make -C $(SNORT_DIR) distclean
+
+	(cd $(SNORT_DIR) ; ./configure $(CONFFLAGS) --with-mysql=$(BT_STAGING_DIR)/usr/ )
+	make $(MAKEOPTS) -C $(SNORT_DIR)
+	cp -a -f $(SNORT_DIR)/src/snort $(SNORT_TARGET_DIR)/usr/bin/snortsql
+	-$(BT_STRIP) $(BT_STRIP_BINOPTS) $(SNORT_TARGET_DIR)/usr/bin/snortsql
+
 	cp -a -f $(SNORT_DIR)/etc/*.conf* $(SNORT_TARGET_DIR)/etc/snort/
 	cp -a -f $(SNORT_DIR)/etc/*.map $(SNORT_TARGET_DIR)/etc/snort/
 	cp -a -f $(SNORT_DIR)/etc/gen* $(SNORT_TARGET_DIR)/etc/snort/
@@ -46,13 +53,6 @@ $(SNORT_DIR)/.build: $(SNORT_DIR)/.source
 	cp -aL -f $(SNORT_INIT) $(SNORT_TARGET_DIR)/etc/init.d/snort
 	chmod 755 $(SNORT_TARGET_DIR)/etc/init.d/snort
 	cp -aL -f $(SNORT_DEFAULT) $(SNORT_TARGET_DIR)/etc/default/snort
-
-	make -C $(SNORT_DIR) distclean
-
-	(cd $(SNORT_DIR) ; CC=$(TARGET_CC) LD=$(TARGET_LD) ./configure $(CONFFLAGS) --with-mysql=$(BT_STAGING_DIR)/usr/ )
-	make -C $(SNORT_DIR) CC=$(TARGET_CC) LD=$(TARGET_LD) CFLAGS="$(BT_COPT_FLAGS)"
-	cp -a -f $(SNORT_DIR)/src/snort $(SNORT_TARGET_DIR)/usr/bin/snortsql
-	$(BT_STRIP) $(BT_STRIP_BINOPTS) $(SNORT_TARGET_DIR)/usr/bin/snortsql
 	cp -a -f $(SNORT_TARGET_DIR)/* $(BT_STAGING_DIR)
 	touch $(SNORT_DIR)/.build
 
