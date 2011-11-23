08586f242e5fd35a8181afed9d882684c5b9ea65
Andrew Denisenko <nitr0man@users.sourceforge.net>
2011-11-23 21:05:20 +0200

    updated iscsi

--- repo/iscsi/buildtool.mk
+++ repo/iscsi/buildtool.mk
@@ -21,13 +21,15 @@ build:
 	mkdir -p $(ISCSI_TARGET_DIR)/etc/modules.d
 	(for i in $(KARCHS); do \
 	mkdir -p $(BT_STAGING_DIR)/lib/modules/$(BT_KERNEL_RELEASE)-$$i/kernel/iscsi && \
-	$(MAKE) KSRC=$(BT_LINUX_DIR)-$$i CC=$(TARGET_CC) -C $(ISCSI_DIR) && \
-	cp -a $(ISCSI_DIR)/kernel/iscsi_trgt.ko $(BT_STAGING_DIR)/lib/modules/$(BT_KERNEL_RELEASE)-$$i/kernel/iscsi && \
-	$(BT_DEPMOD) -ae -b $(BT_STAGING_DIR) -r -F $(BT_STAGING_DIR)/lib/modules/$(BT_KERNEL_RELEASE)-$$i/build/System.map $(BT_KERNEL_RELEASE)-$$i ; \
+	$(MAKE) $(MAKEOPTS) KSRC=$(BT_LINUX_DIR)-$$i CC=$(TARGET_CC) -C $(ISCSI_DIR) && \
+	gzip -9 $(ISCSI_DIR)/kernel/iscsi_trgt.ko && \
+	cp -a $(ISCSI_DIR)/kernel/iscsi_trgt.ko.gz $(BT_STAGING_DIR)/lib/modules/$(BT_KERNEL_RELEASE)-$$i/kernel/iscsi && \
+	rm -f $(ISCSI_DIR)/kernel/iscsi_trgt.ko.gz && \
+	depmod -ae -b $(BT_STAGING_DIR) -r -F $(BT_STAGING_DIR)/lib/modules/$(BT_KERNEL_RELEASE)-$$i/build/System.map $(BT_KERNEL_RELEASE)-$$i ; \
 	done)
 	cp -a $(ISCSI_DIR)/usr/ietd $(ISCSI_TARGET_DIR)/usr/sbin
 	cp -a $(ISCSI_DIR)/usr/ietadm $(ISCSI_TARGET_DIR)/usr/sbin
-	$(BT_STRIP) $(BT_STRIP_BINOPTS) $(ISCSI_TARGET_DIR)/usr/sbin/*
+	-$(BT_STRIP) $(BT_STRIP_BINOPTS) $(ISCSI_TARGET_DIR)/usr/sbin/*
 	cp -a $(ISCSI_DIR)/etc/*.* $(ISCSI_TARGET_DIR)/etc
 	cp -aL iscsi-target.init $(ISCSI_TARGET_DIR)/etc/init.d/iscsi-target
 	cp -aL iscsid.modules $(ISCSI_TARGET_DIR)/etc/modules.d
