5d23ef6a58439fa528fab6b989118cd91de91ec6
Andrew Denisenko <nitr0man@users.sourceforge.net>
2011-11-24 18:12:11 +0200

    updated portmap

--- repo/portmap/buildtool.mk
+++ repo/portmap/buildtool.mk
@@ -24,16 +24,11 @@ source: $(PORTMAP_DIR)/.source
 
 build: source
 	mkdir -p $(PORTMAP_TARGET_DIR)/sbin
-	mkdir -p $(PORTMAP_TARGET_DIR)/usr/share/man/man8
-	$(MAKE) CC=$(TARGET_CC) LD=$(TARGET_LD) -C $(PORTMAP_DIR)
+	$(MAKE) $(MAKEOPTS) CC=$(TARGET_CC) LD=$(TARGET_LD) -C $(PORTMAP_DIR)
 	$(MAKE) -C $(PORTMAP_DIR) BASEDIR=$(PORTMAP_TARGET_DIR) install
 #
-	$(BT_STRIP) $(BT_STRIP_BINOPTS) $(PORTMAP_TARGET_DIR)/sbin/portmap
-	cp -f $(PORTMAP_TARGET_DIR)/sbin/portmap $(BT_STAGING_DIR)/sbin
-	$(BT_STRIP) $(BT_STRIP_BINOPTS) $(PORTMAP_TARGET_DIR)/sbin/pmap_set
-	cp -f $(PORTMAP_TARGET_DIR)/sbin/pmap_set $(BT_STAGING_DIR)/sbin
-	$(BT_STRIP) $(BT_STRIP_BINOPTS) $(PORTMAP_TARGET_DIR)/sbin/pmap_dump
-	cp -f $(PORTMAP_TARGET_DIR)/sbin/pmap_dump $(BT_STAGING_DIR)/sbin
+	-$(BT_STRIP) $(BT_STRIP_BINOPTS) $(PORTMAP_TARGET_DIR)/sbin/*
+	cp -af $(PORTMAP_TARGET_DIR)/sbin/* $(BT_STAGING_DIR)/sbin/
 	mkdir -p $(BT_STAGING_DIR)/etc/default/
 	mkdir -p $(BT_STAGING_DIR)/etc/init.d/
 	cp -aL portmap.default $(BT_STAGING_DIR)/etc/default/portmap
