301a71f8a604f8fc720ddbbf5b9c6a77f38d9f31
Andrew Denisenko <nitr0man@users.sourceforge.net>
2011-11-25 23:50:22 +0200

    updated libnfsidmap and nfs-utils

--- repo/nfs-utils/buildtool.mk
+++ repo/nfs-utils/buildtool.mk
@@ -16,7 +16,7 @@ NFSUTILS_TARGET_DIR:=$(BT_BUILD_DIR)/nfs-utils
 #   Disable tiprc to avoid build failure checking for clnt_tli_create()
 #   Disable uuid to avoid the need for libblkid
 #   Disable gss to avoid the need for libgssglue etc.
-CONFOPTS:= --build=$(GNU_TARGET_NAME) --host=$(GNU_HOST_NAME) \
+CONFOPTS:= --host=$(GNU_TARGET_NAME) \
 	--disable-tirpc --disable-uuid --disable-gss
 
 $(NFSUTILS_DIR)/.source:
@@ -37,31 +37,19 @@ $(NFSUTILS_DIR)/.configure: $(NFSUTILS_DIR)/.source
 
 build: $(NFSUTILS_DIR)/.configure
 	mkdir -p $(NFSUTILS_TARGET_DIR)
-	$(MAKE) CC=$(TARGET_CC) LD=$(TARGET_LD) -C $(NFSUTILS_DIR)
+	mkdir -p $(NFSUTILS_TARGET_DIR)/etc/default
+	mkdir -p $(NFSUTILS_TARGET_DIR)/etc/init.d
+	$(MAKE) $(MAKEOPTS) -C $(NFSUTILS_DIR)
 	$(MAKE) -C $(NFSUTILS_DIR) DESTDIR=$(NFSUTILS_TARGET_DIR) install
 #
-	$(BT_STRIP) $(BT_STRIP_BINOPTS) $(NFSUTILS_TARGET_DIR)/usr/sbin/exportfs
-	cp -f $(NFSUTILS_TARGET_DIR)/usr/sbin/exportfs $(BT_STAGING_DIR)/usr/sbin
-	$(BT_STRIP) $(BT_STRIP_BINOPTS) $(NFSUTILS_TARGET_DIR)/usr/sbin/nfsstat
-	cp -f $(NFSUTILS_TARGET_DIR)/usr/sbin/nfsstat $(BT_STAGING_DIR)/usr/sbin
-	$(BT_STRIP) $(BT_STRIP_BINOPTS) $(NFSUTILS_TARGET_DIR)/usr/sbin/rpc.mountd
-	cp $(NFSUTILS_TARGET_DIR)/usr/sbin/rpc.mountd $(BT_STAGING_DIR)/usr/sbin
-	$(BT_STRIP) $(BT_STRIP_BINOPTS) $(NFSUTILS_TARGET_DIR)/usr/sbin/rpc.nfsd
-	cp -f $(NFSUTILS_TARGET_DIR)/usr/sbin/rpc.nfsd $(BT_STAGING_DIR)/usr/sbin
-	$(BT_STRIP) $(BT_STRIP_BINOPTS) $(NFSUTILS_TARGET_DIR)/usr/sbin/rpc.statd
-	cp -f $(NFSUTILS_TARGET_DIR)/usr/sbin/rpc.statd $(BT_STAGING_DIR)/usr/sbin
-	$(BT_STRIP) $(BT_STRIP_BINOPTS) $(NFSUTILS_TARGET_DIR)/usr/sbin/rpc.idmapd
-	cp -f $(NFSUTILS_TARGET_DIR)/usr/sbin/rpc.idmapd $(BT_STAGING_DIR)/usr/sbin
-	$(BT_STRIP) $(BT_STRIP_BINOPTS) $(NFSUTILS_TARGET_DIR)/usr/sbin/showmount
-	cp -f $(NFSUTILS_TARGET_DIR)/usr/sbin/showmount $(BT_STAGING_DIR)/usr/sbin
-	$(BT_STRIP) $(BT_STRIP_BINOPTS) $(NFSUTILS_TARGET_DIR)/usr/sbin/sm-notify
-	cp -f $(NFSUTILS_TARGET_DIR)/usr/sbin/sm-notify $(BT_STAGING_DIR)/usr/sbin
-	mkdir -p $(BT_STAGING_DIR)/etc/default/
-	mkdir -p $(BT_STAGING_DIR)/etc/init.d/
-	cp -aL nfs-utils.default $(BT_STAGING_DIR)/etc/default/nfs-utils
-	cp -aL nfs-utils.exports $(BT_STAGING_DIR)/etc/exports
-	cp -aL nfs-utils.init $(BT_STAGING_DIR)/etc/init.d/nfs-utils
-	cp -aL idmapd.conf $(BT_STAGING_DIR)/etc/
+	-$(BT_STRIP) $(BT_STRIP_BINOPTS) $(NFSUTILS_TARGET_DIR)/usr/sbin/*
+	-$(BT_STRIP) $(BT_STRIP_BINOPTS) $(NFSUTILS_TARGET_DIR)/sbin/*
+	cp -aL nfs-utils.default $(NFSUTILS_TARGET_DIR)/etc/default/nfs-utils
+	cp -aL nfs-utils.exports $(NFSUTILS_TARGET_DIR)/etc/exports
+	cp -aL nfs-utils.init $(NFSUTILS_TARGET_DIR)/etc/init.d/nfs-utils
+	cp -aL idmapd.conf $(NFSUTILS_TARGET_DIR)/etc/
+	-rm -rf $(NFSUTILS_TARGET_DIR)/usr/share
+	cp -a $(NFSUTILS_TARGET_DIR)/* $(BT_STAGING_DIR)/
 
 clean:
 	rm -rf $(NFSUTILS_TARGET_DIR)
