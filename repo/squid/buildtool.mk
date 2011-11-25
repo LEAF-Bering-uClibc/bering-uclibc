3d7eb7907bd11a752d7fa9383ce005e40145c513
Andrew Denisenko <nitr0man@users.sourceforge.net>
2011-11-25 22:17:35 +0200

    updated squid and speedtouch

--- repo/squid/buildtool.mk
+++ repo/squid/buildtool.mk
@@ -5,6 +5,7 @@ include $(MASTERMAKEFILE)
 
 SQUID_DIR:=squid-3.2.0.12
 SQUID_TARGET_DIR:=$(BT_BUILD_DIR)/squid
+export HOSTCXX=g++
 
 $(SQUID_DIR)/.source:
 	zcat $(SQUID_SOURCE) | tar -xvf -
@@ -67,12 +68,6 @@ $(SQUID_DIR)/.build: $(SQUID_DIR)/.configured
 #	make CFLAGS="$(BT_COPT_FLAGS)" -C $(SQUID_DIR) all
 	make $(MAKEOPTS) -C $(SQUID_DIR) all
 	chmod 644 $(SQUID_DIR)/icons/silk/*
-	-$(BT_STRIP) -s --remove-section=.note --remove-section=.comment $(SQUID_DIR)/src/squid
-	-$(BT_STRIP) -s --remove-section=.note --remove-section=.comment $(SQUID_DIR)/tools/squidclient
-#	-$(BT_STRIP) -s --remove-section=.note --remove-section=.comment $(SQUID_DIR)/src/dnsserver
-	-$(BT_STRIP) -s --remove-section=.note --remove-section=.comment $(SQUID_DIR)/src/unlinkd
-	-$(BT_STRIP) -s --remove-section=.note --remove-section=.comment $(SQUID_DIR)/tools/cachemgr.cgi
-	-$(BT_STRIP) -s --remove-section=.note --remove-section=.comment $(SQUID_DIR)/helpers/log_daemon/file/log_file_daemon
 	cp -aL squid.init $(SQUID_TARGET_DIR)/etc/init.d/squid
 	cp -aL squid.conf $(SQUID_TARGET_DIR)/etc/squid
 	cp -aL ok_domains $(SQUID_TARGET_DIR)/etc/squid
@@ -89,6 +84,8 @@ $(SQUID_DIR)/.build: $(SQUID_DIR)/.configured
 	cp -a $(SQUID_DIR)/src/unlinkd $(SQUID_TARGET_DIR)/usr/bin
 	cp -a $(SQUID_DIR)/helpers/log_daemon/file/log_file_daemon $(SQUID_TARGET_DIR)/usr/bin
 	cp -a $(SQUID_DIR)/tools/cachemgr.cgi $(SQUID_TARGET_DIR)/usr/lib/cgi-bin
+	-$(BT_STRIP) $(BT_STRIP_BINOPTS) $(SQUID_TARGET_DIR)/usr/bin/*
+	-$(BT_STRIP) $(BT_STRIP_BINOPTS) $(SQUID_TARGET_DIR)/usr/lib/cgi-bin/*
 	cp -a $(SQUID_TARGET_DIR)/* $(BT_STAGING_DIR)
 	touch $(SQUID_DIR)/.build
 
