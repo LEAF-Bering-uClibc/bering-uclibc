133a2a347b317c2d796ee4b9e25d1c3dedbbbf5d
Andrew Denisenko <nitr0man@users.sourceforge.net>
2011-11-26 01:00:56 +0200

    updated asterisk

--- repo/asterisk/buildtool.mk
+++ repo/asterisk/buildtool.mk
@@ -15,32 +15,31 @@ ASTERISK_TARGET_DIR:=$(BT_BUILD_DIR)/asterisk
 # Option settings for 'configure':
 #   Move files out from under /usr/local/
 #   Disable generation of XML documentation
-CONFOPTS:=--host=i486-pc-linux-gnu \
+CONFOPTS:=--host=$(GNU_TARGET_NAME) \
 	--prefix=/usr --disable-xmldoc --without-sdl
 # --enable-dev-mode
 
 .source:
 	zcat $(SOURCE) | tar -xvf -
 	echo $(ASTERISK_DIR) > DIRNAME
+	perl -i -p -e 's,\&\&\s+\./configure, ,' $(ASTERISK_DIR)/configure $(ASTERISK_DIR)/configure.ac
 	touch .source
 
 source: .source
 
 .configure: .source
 	( cd $(ASTERISK_DIR) ; ./configure $(CONFOPTS) )
+	( cd $(ASTERISK_DIR)/menuselect ; ./configure $(CONFOPTS) --host=$(GNU_TARGET_NAME) )
 	touch .configure
 
 build: .configure
 	mkdir -p $(ASTERISK_TARGET_DIR)
-	$(MAKE) -C $(ASTERISK_DIR) \
-		CC=$(TARGET_CC) \
-		LD=$(TARGET_LD) \
-		OPTIONS="-DLOW_MEMORY" \
-		DESTDIR=$(ASTERISK_TARGET_DIR) install samples
+	$(MAKE) $(MAKEOPTS) -C $(ASTERISK_DIR) OPTIONS="-DLOW_MEMORY"
+	$(MAKE) -C $(ASTERISK_DIR) DESTDIR=$(ASTERISK_TARGET_DIR) install samples
 #
-	$(BT_STRIP) $(BT_STRIP_LIBOPTS) $(ASTERISK_TARGET_DIR)/usr/lib/asterisk/modules/*.so
-	$(BT_STRIP) $(BT_STRIP_BINOPTS) $(ASTERISK_TARGET_DIR)/usr/sbin/asterisk
-	$(BT_STRIP) $(BT_STRIP_BINOPTS) $(ASTERISK_TARGET_DIR)/usr/sbin/astcanary
+	-$(BT_STRIP) $(BT_STRIP_LIBOPTS) $(ASTERISK_TARGET_DIR)/usr/lib/asterisk/modules/*
+	-$(BT_STRIP) $(BT_STRIP_BINOPTS) $(ASTERISK_TARGET_DIR)/usr/sbin/*
+	-rm -rf $(ASTERISK_TARGET_DIR)/usr/share
 	mkdir -p $(BT_STAGING_DIR)/etc/init.d/
 	cp -L -f asterisk.init $(BT_STAGING_DIR)/etc/init.d/asterisk
 	cp -a -f $(ASTERISK_TARGET_DIR)/etc/asterisk $(BT_STAGING_DIR)/etc
