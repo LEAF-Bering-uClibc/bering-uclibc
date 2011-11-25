0269b20eba413e0601dd6e46251fbd45d6449744
Andrew Denisenko <nitr0man@users.sourceforge.net>
2011-11-25 22:20:47 +0200

    updated p9100

--- repo/p9100/buildtool.mk
+++ repo/p9100/buildtool.mk
@@ -18,11 +18,11 @@ $(P9100_DIR)/.build: $(P9100_DIR)/.configured
 	mkdir -p $(P9100_TARGET_DIR)/etc/init.d
 	mkdir -p $(P9100_TARGET_DIR)/etc/default
 	mkdir -p $(P9100_TARGET_DIR)/usr/sbin
-	make CC=$(TARGET_CC) -C $(P9100_DIR) CFLAGS="$(BT_COPT_FLAGS) -DLOCKFILE_DIR=\\"\"/var/lock\\"\""
-	-$(BT_STRIP) $(BT_STRIP_BINOPTS) $(P9100_DIR)/p910nd
+	make CROSS=$(CROSS_COMPILE) -C $(P9100_DIR) CFLAGS="$(CFLAGS) -DLOCKFILE_DIR=\\"\"/var/lock\\"\""
 	cp -a $(P9100_DIR)/p910nd $(P9100_TARGET_DIR)/usr/sbin
 	cp -aL p910nd.init $(P9100_TARGET_DIR)/etc/init.d/p910nd
 	cp -aL p910nd.default $(P9100_TARGET_DIR)/etc/default/p910nd
+	-$(BT_STRIP) $(BT_STRIP_BINOPTS) $(P9100_TARGET_DIR)/usr/sbin/*
 	cp -a $(P9100_TARGET_DIR)/* $(BT_STAGING_DIR)
 	touch $(P9100_DIR)/.build
 
