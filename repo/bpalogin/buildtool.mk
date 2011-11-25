9b3d5d78a6054ccc838e81a7b7db8cb0c40dc86f
Andrew Denisenko <nitr0man@users.sourceforge.net>
2011-11-25 22:23:47 +0200

    updated bpalogin

--- repo/bpalogin/buildtool.mk
+++ repo/bpalogin/buildtool.mk
@@ -11,18 +11,19 @@ $(BPALOGIN_DIR)/.source:
 source: $(BPALOGIN_DIR)/.source
 
 $(BPALOGIN_DIR)/.configured: $(BPALOGIN_DIR)/.source
-	(cd $(BPALOGIN_DIR) ; CC=$(TARGET_CC) LD=$(TARGET_LD) CFLAGS="$(BT_COPT_FLAGS)" ./configure --prefix=/usr )
+	(cd $(BPALOGIN_DIR) ; ./configure --prefix=/usr \
+	--host=$(GNU_TARGET_NAME) )
 	touch $(BPALOGIN_DIR)/.configured
 
 $(BPALOGIN_DIR)/.build: $(BPALOGIN_DIR)/.configured
 	mkdir -p $(BPALOGIN_TARGET_DIR)
 	mkdir -p $(BPALOGIN_TARGET_DIR)/etc/init.d
 	mkdir -p $(BPALOGIN_TARGET_DIR)/usr/sbin
-	make -C $(BPALOGIN_DIR) DESTDIR=$(BPALOGIN_TARGET_DIR)
-	-$(BT_STRIP) -s --remove-section=.note --remove-section=.comment $(BPALOGIN_DIR)/bpalogin
+	make $(MAKEOPTS) -C $(BPALOGIN_DIR) DESTDIR=$(BPALOGIN_TARGET_DIR)
 	cp -aL bpalogin.init $(BPALOGIN_TARGET_DIR)/etc/init.d/bpalogin
 	cp -a $(BPALOGIN_DIR)/bpalogin.conf $(BPALOGIN_TARGET_DIR)/etc/
 	cp -a $(BPALOGIN_DIR)/bpalogin $(BPALOGIN_TARGET_DIR)/usr/sbin
+	-$(BT_STRIP) $(BT_STRIP_BINOPTS) $(BPALOGIN_TARGET_DIR)/usr/sbin/*
 	cp -a $(BPALOGIN_TARGET_DIR)/* $(BT_STAGING_DIR)
 	touch $(BPALOGIN_DIR)/.build
 
