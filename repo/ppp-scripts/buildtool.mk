b16f1cd01a6ac2e07cf8ec044b351b97fcda8c2d
Andrew Denisenko <nitr0man@users.sourceforge.net>
2012-01-06 20:28:54 +0200

    Moved ip-down/ip-mod/ip-up to separate package

---
+++ repo/ppp-scripts/buildtool.mk
@@ -0,0 +1,19 @@
+# makefile for ppp, ppp-filter, pppoe and pppoatm
+include $(MASTERMAKEFILE)
+
+PPP_SCRIPT_TARGET_DIR:=$(BT_BUILD_DIR)/ppp-scripts
+
+source:
+
+build:
+	mkdir -p $(PPP_SCRIPT_TARGET_DIR)/etc/ppp
+	cp -aL ip-up $(PPP_SCRIPT_TARGET_DIR)/etc/ppp
+	cp -aL ip-mod $(PPP_SCRIPT_TARGET_DIR)/etc/ppp
+	cp -aL ip-down $(PPP_SCRIPT_TARGET_DIR)/etc/ppp
+	cp -aL ipv6-up $(PPP_SCRIPT_TARGET_DIR)/etc/ppp
+	cp -aL ipv6-down $(PPP_SCRIPT_TARGET_DIR)/etc/ppp
+
+clean:
+	rm -rf $(PPP_SCRIPT_TARGET_DIR)
+
+srcclean: clean
