b3c8022d5e53dbf4b1ca642f84c12184be433ba4
Andrew Denisenko <nitr0man@users.sourceforge.net>
2012-01-08 00:57:59 +0200

    fix

--- repo/ppp-scripts/buildtool.mk
+++ repo/ppp-scripts/buildtool.mk
@@ -12,6 +12,7 @@ build:
 	cp -aL ip-down $(PPP_SCRIPT_TARGET_DIR)/etc/ppp
 	cp -aL ipv6-up $(PPP_SCRIPT_TARGET_DIR)/etc/ppp
 	cp -aL ipv6-down $(PPP_SCRIPT_TARGET_DIR)/etc/ppp
+	cp -a $(PPP_SCRIPT_TARGET_DIR)/* $(BT_STAGING_DIR)
 
 clean:
 	rm -rf $(PPP_SCRIPT_TARGET_DIR)
