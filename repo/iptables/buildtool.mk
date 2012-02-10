e868794f89b08ece981b0056823208226820d9b8
kapeka <kapeka@users.sourceforge.net>
2012-02-10 18:28:33 +0100

    downgrade iptables to 1.4.10, so it can be build

--- repo/iptables/buildtool.mk
+++ repo/iptables/buildtool.mk
@@ -6,7 +6,7 @@
 #############################################################
 
 include $(MASTERMAKEFILE)
-IPTABLES_VER=1.4.12.2
+IPTABLES_VER=1.4.10
 IPTABLES_DIR:=iptables-$(IPTABLES_VER)
 IPT_NF_DIR:=ipt_netflow-1.7.1
 IPTABLES_TARGET_DIR:=$(BT_BUILD_DIR)/iptables
