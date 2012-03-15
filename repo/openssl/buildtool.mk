41c459032b074ab17f399d981d1021e90e327382
kapeka <kapeka@users.sourceforge.net>
2012-03-15 17:01:06 +0100

    openssl update to 1.0.0h

--- repo/openssl/buildtool.mk
+++ repo/openssl/buildtool.mk
@@ -6,7 +6,7 @@
 #############################################################
 
 include $(MASTERMAKEFILE)
-OPENSSL_DIR:=openssl-1.0.0g
+OPENSSL_DIR:=openssl-1.0.0h
 OPENSSL_TARGET_DIR:=$(BT_BUILD_DIR)/openssl
 
 PERLVER=$(shell ls $(BT_STAGING_DIR)/usr/lib/perl5) 
