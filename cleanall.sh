a647bb61a115938c376512ff8355029c02e86c26
david M brooke <davidmbrooke@users.sourceforge.net>
2012-03-21 22:19:28 +0000

    cleanall.sh must now delete conf/*/installed

--- cleanall.sh
+++ cleanall.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-rm -rf package source staging toolchain log build conf/installed
+rm -rf package source staging toolchain log build conf/installed conf/*/installed
 find image/ -name *.gz -delete
 find image/ -name *.iso -delete
