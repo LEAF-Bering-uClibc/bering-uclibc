78a6af736e9262a4b6097f6448ad61c0e3fd8902
david M brooke <davidmbrooke@users.sourceforge.net>
2012-03-24 08:37:24 +0000

    fixed mawk for cross-compiled build

--- repo/mawk/buildtool.mk
+++ repo/mawk/buildtool.mk
@@ -1,40 +1,52 @@
-# makefile for mawk
+#############################################################
+#
+# buildtool makefile for mawk
+#
+#############################################################
+
 include $(MASTERMAKEFILE)
 
-MAWK_DIR:=mawk-1.3.4-20100625
+MAWK_DIR:=$(shell $(BT_TGZ_GETDIRNAME) $(SOURCE) 2>/dev/null )
+ifeq ($(MAWK_DIR),)
+MAWK_DIR:=$(shell cat DIRNAME)
+endif
 MAWK_TARGET_DIR:=$(BT_BUILD_DIR)/mawk
 
-$(MAWK_DIR)/.source:
-	zcat $(MAWK_SOURCE) | tar -xvf -
-	zcat $(MAWK_PATCH) | patch -d $(MAWK_DIR) -p1
-	(cd $(MAWK_DIR); export DEB_BUILD_ARCH=i386; $(BT_DPATCH) apply-all;)
-	touch $(MAWK_DIR)/.source
+.source:
+	zcat $(SOURCE) | tar -xvf -
+	echo $(MAWK_DIR) > DIRNAME
+	# Hack Makefile.in for cross-compiled build
+	# Program makescan.exe needs to execute on the *build* host
+	perl -i -p -e "s,^.* -o makescan\.exe .*,	gcc \\$(CPPFLAGS) -o makescan.exe makescan.c," ${MAWK_DIR}/Makefile.in
+	touch .source
 
-source: $(MAWK_DIR)/.source
+source: .source
 
-$(MAWK_DIR)/.configured: $(MAWK_DIR)/.source
+.configure: .source
 	(cd $(MAWK_DIR) ; CC=$(TARGET_CC) ./configure --host=$(GNU_TARGET_NAME) --build=$(GNU_BUILD_NAME))
-	touch $(MAWK_DIR)/.configured
+	touch .configure
 
-$(MAWK_DIR)/.build: $(MAWK_DIR)/.configured
+.build: .configure
 	mkdir -p $(MAWK_TARGET_DIR)
 	mkdir -p $(MAWK_TARGET_DIR)/usr/bin
 	perl -i -p -e "s,trap 0,trap - 0,g" ${MAWK_DIR}/test/mawktest
 	perl -i -p -e "s,\strap\s*0,trap - 0,g" ${MAWK_DIR}/test/fpe_test
-# Multi-threaded make fails
+	# Multi-threaded make fails so don't specify $(MAKEOPTS)
 	make -C $(MAWK_DIR) mawk
 	cp -a $(MAWK_DIR)/mawk $(MAWK_TARGET_DIR)/usr/bin
 	-$(BT_STRIP) $(BT_STRIP_BINOPTS) $(MAWK_TARGET_DIR)/usr/bin/*
 	cp -a $(MAWK_TARGET_DIR)/* $(BT_STAGING_DIR)
-	touch $(MAWK_DIR)/.build
+	touch .build
 
-build: $(MAWK_DIR)/.build
+build: .build
 
 clean:
+	make -C $(MAWK_DIR) clean
 	rm -rf $(MAWK_TARGET_DIR)
-	rm -f $(MAWK_DIR)/.build
-	rm -f $(MAWK_DIR)/.configured
+	rm -f .build
+	rm -f .configure
 
 srcclean: clean
 	rm -rf $(MAWK_DIR)
-	rm -f $(MAWK_DIR)/.source
+	rm DIRNAME
+	rm -f .source
