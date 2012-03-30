a31ec557585b3f6b6314596d06dd110a758c6f31
Andrew Denisenko <nitr0man@users.sourceforge.net>
2012-03-30 22:34:42 +0300

    Small changes in initrd scripts

--- repo/initrd/buildtool.mk
+++ repo/initrd/buildtool.mk
@@ -16,9 +16,10 @@ $(INITRD_DIR)/.build:
 	echo $(ESCKEY) "isofs\nvfat">$(INITRD_TARGET_DIR)/boot/etc/modules
 	-rm -f package.cfg
 	(for a in $(KARCHS); do \
+	sed 's,#.*$$,\n,' modulelist.common >modulelist.$$a ; \
+	[ -f specific.$$a ] && sed 's,#.*$$,\n,' specific.$$a >>modulelist.$$a ; \
 	BT_STAGING_DIR=$(BT_STAGING_DIR) BT_KERNEL_RELEASE=$(BT_KERNEL_RELEASE)-$$a \
-		    sh $(BT_TOOLS_DIR)/getdep.sh "ata_.*" ahci ehci-hcd uhci-hcd \
-		    ohci-hcd usb-storage sd_mod sr_mod isofs vfat floppy usbhid >mod ; \
+		    sh $(BT_TOOLS_DIR)/getdep.sh `cat modulelist.$$a` >mod ; \
 	[ -f files.$$a ] && rm -f files.$$a ; \
 	for m in `cat mod`; do echo $(ESCKEY) "<File>\n\tSource\t\t= lib/modules/__KVER__-$$a/$$m \n\t\
 	Filename\t= lib/modules/$$(echo $$m|sed 's,\([a-z0-9]*/\)\+,,')\n\t\
