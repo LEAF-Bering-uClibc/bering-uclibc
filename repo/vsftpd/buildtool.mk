61b4bd71cbd2774002fb0fe2bd2da38fe5e30eba
kapeka <kapeka@users.sourceforge.net>
2012-04-22 15:56:05 +0200

    vsftpd update to 3.0.0

--- repo/vsftpd/buildtool.mk
+++ repo/vsftpd/buildtool.mk
@@ -6,7 +6,7 @@
 #############################################################
 
 include $(MASTERMAKEFILE)
-VSFTPD_DIR:=vsftpd-2.3.4
+VSFTPD_DIR:=vsftpd-3.0.0
 VSFTPD_TARGET_DIR:=$(BT_BUILD_DIR)/vsftpd
 
 
@@ -23,7 +23,7 @@ $(VSFTPD_DIR)/.build:
 	mkdir -p $(VSFTPD_TARGET_DIR)/usr/sbin
 	mkdir -p $(VSFTPD_TARGET_DIR)/etc/init.d
 	mkdir -p $(VSFTPD_TARGET_DIR)/etc/cron.daily
-	$(MAKE) $(MAKEOPTS) -C $(VSFTPD_DIR) CC=$(TARGET_CC) CFLAGS="-Wall -W -Wshadow $(CFLAGS)" LINK=
+	$(MAKE) $(MAKEOPTS) -C $(VSFTPD_DIR) CC=$(TARGET_CC) LDFLAGS="-fPIE -pie -Wl,-z,relro -Wl,-z,now  $(LDFLAGS)" CFLAGS="-Wall -W -Wshadow $(CFLAGS)" LINK=
 	cp -a $(VSFTPD_DIR)/vsftpd $(VSFTPD_TARGET_DIR)/usr/sbin/
 	cp -aL vsftpd.init $(VSFTPD_TARGET_DIR)/etc/init.d/vsftpd
 	cp -aL vsftpd.conf $(VSFTPD_TARGET_DIR)/etc/
