cd145a4c6035feb5fd1cb16a88a4914cc3d8dcbd
kapeka <kapeka@users.sourceforge.net>
2012-04-28 00:50:47 +0200

    kmodules don't add "type=module" avoid  genaration of moddb-*.modules
    Fixes the removal of every packaged module when saving moddb with apkg.

--- repo/kmodules/buildtool.mk
+++ repo/kmodules/buildtool.mk
@@ -23,7 +23,7 @@ build:
 	[ -f files.$$i ] && rm -f files.$$i ; \
 	for m in `cat mod`; do echo $(ESCKEY) "<File>\n\tSource\t\t= lib/modules/__KVER__-$$i/$$m \n\t\
 	Filename\t= lib/modules/$$(echo $$m|sed 's,\([a-z0-9_-]*/\)\+,,')\n\t\
-	Type\t\t= binary\n\tType\t\t= module\n\tPermissions\t= 644\n</File>">>files.$$i; done; \
+	Type\t\t= binary\n\t\tPermissions\t= 644\n</File>">>files.$$i; done; \
 	echo $(ESCKEY) "#include <common.$$i>" >>package.cfg; \
 	sed 's,##KARCH##,'"$$i"',g' common.tpl >common.$$i ; \
 	done)
