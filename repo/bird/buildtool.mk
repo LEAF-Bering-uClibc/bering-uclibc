2d921375ce739613f3646559831ca2928cd819ca
Andrew Denisenko <nitr0man@users.sourceforge.net>
2012-06-17 19:07:14 +0300

    fixed bird (earlier it was built only for IPv6)

--- repo/bird/buildtool.mk
+++ repo/bird/buildtool.mk
@@ -9,8 +9,22 @@ $(DIR)/.source:
 	touch $(DIR)/.source
 
 source: $(DIR)/.source
-                        
-$(DIR)/.configured: $(DIR)/.source
+
+$(DIR)/.build: $(DIR)/.source
+	mkdir -p $(TARGET_DIR)/etc/init.d
+	(cd $(DIR) ; \
+	CC=$(TARGET_CC) LD=$(TARGET_LD) \
+	CFLAGS="$(BT_COPT_FLAGS)" \
+	LDFLAGS="-L$(BT_STAGING_DIR)/lib -L$(BT_STAGING_DIR)/usr/lib $(LDFLAGS)" \
+	./configure prefix=/usr \
+	--sysconfdir=/etc/bird \
+	--localstatedir=/var \
+	--build=$(GNU_HOST_MANE) \
+	--host=$(GNU_TARGET_MANE) \
+	--with-iproutedir="$(BT_STAGING_DIR)/etc/iproute2")
+	make -C $(DIR) all
+	make DESTDIR=$(TARGET_DIR) -C $(DIR) install
+	make -C $(DIR) clean
 	(cd $(DIR) ; \
 	CC=$(TARGET_CC) LD=$(TARGET_LD) \
 	CFLAGS="$(BT_COPT_FLAGS)" \
@@ -22,28 +36,27 @@ $(DIR)/.configured: $(DIR)/.source
 	--host=$(GNU_TARGET_MANE) \
 	--with-iproutedir="$(BT_STAGING_DIR)/etc/iproute2" \
 	--enable-ipv6)
-	touch $(DIR)/.configured
-#	--disable-client \
-#	--with-sysinclude="$(BT_STAGING_DIR)/include" \
-                                                                 
-$(DIR)/.build: $(DIR)/.configured
-	mkdir -p $(TARGET_DIR)/etc/init.d
 	make -C $(DIR) all
 	make DESTDIR=$(TARGET_DIR) -C $(DIR) install
+	make -C $(DIR) clean
 	-$(BT_STRIP) $(BT_STRIP_BINOPTS) $(TARGET_DIR)/usr/sbin/*
 	cp -aL bird.init $(TARGET_DIR)/etc/init.d/bird
+	cp -aL bird.init $(TARGET_DIR)/etc/init.d/bird6
 	cp -a $(TARGET_DIR)/* $(BT_STAGING_DIR)
 	touch $(DIR)/.build
 
 build: $(DIR)/.build
-                                                                                         
+
 clean:
 	make -C $(DIR) clean
 	-rm -rf $(TARGET_DIR)
 	-rm $(BT_STAGING_DIR)/usr/sbin/bird6
 	-rm $(BT_STAGING_DIR)/usr/sbin/birdc6
+	-rm $(BT_STAGING_DIR)/usr/sbin/bird
+	-rm $(BT_STAGING_DIR)/usr/sbin/birdc
 	-rm -rf $(BT_STAGING_DIR)/etc/bird
 	-rm $(BT_STAGING_DIR)/etc/init.d/bird
+	-rm $(BT_STAGING_DIR)/etc/init.d/bird6
 	-rm -rf $(DIR)/.build
 	-rm -rf $(DIR)/.configured
                                                                                                                  
