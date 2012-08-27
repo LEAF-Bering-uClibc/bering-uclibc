d0f0a48d835dccdbac698a48b7dccf05643b98b6
Andrew Denisenko <nitr0man@users.sourceforge.net>
2012-08-27 15:51:46 +0300

    Hotplug helper: added hardware re-check after device unplugging (reason: modprobe -r can unload required modules, for ex., unplugging of usb flash causes sd_mod removing)

--- repo/initrd/hotplug.sh
+++ repo/initrd/hotplug.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
 [ "$ACTION" = add ] && [ "$MODALIAS" != "" ] && modprobe $MODALIAS
-[ "$ACTION" = remove ] && [ "$MODALIAS" != "" ] && modprobe -r $MODALIAS
+if [ "$ACTION" = remove ] && [ "$MODALIAS" != "" ]; then
+	modprobe -r $MODALIAS
+	# modprobe can remove actually needed modules, so we should
+	# touch uevent for load all required modules for cold-plugged devices
+	find /sys -type f -name 'uevent' | while read dev_event; do
+		echo 'add' > $dev_event
+	done
+fi
 
 /sbin/mdev $@
