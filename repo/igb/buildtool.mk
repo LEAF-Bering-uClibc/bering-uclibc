388ab070d33e2e0de1ac1110bf5d523a290f30af
Yves Blusseau <blusseau@users.sourceforge.net>
2012-09-06 11:43:46 +0200

    Fix and upgrade igb compilation

--- repo/igb/buildtool.mk
+++ repo/igb/buildtool.mk
@@ -1,24 +1,23 @@
-# makefile for accel-pptp
+# makefile for igb
 include $(MASTERMAKEFILE)
 
-DIR:=igb-3.3.6
+DIR:=$(CURDIR)/$(shell $(BT_TGZ_GETDIRNAME) $(SOURCE) 2>/dev/null )
 TARGET_DIR:=$(BT_BUILD_DIR)/igb
 MODULE_PATH=kernel/drivers/net/ethernet/intel/igb
 EXTRA_VARS=$(MAKEOPTS)
 
 $(DIR)/.source:
-	zcat $(SOURCE) | tar -xvf -
+	$(BT_SETUP_BUILDDIR) -v $(SOURCE)
 	cat $(PATCH1) | patch -p1 -d $(DIR)
 	touch $(DIR)/.source
 
 source: $(DIR)/.source
 
 $(DIR)/.build:
-	(cd $(DIR)/src && for i in $(KARCHS_PCIE); do export LOCALVERSION="-$$i" ; \
-	mkdir -p $(TARGET_DIR)/lib/modules/$(BT_KERNEL_RELEASE)-$$i/$(MODULE_PATH) ; \
-	$(MAKE) $(EXTRA_VARS) KSRC=$(BT_LINUX_DIR)-$$i CC=$(TARGET_CC) clean && \
+	(cd $(DIR)/src && for i in $(KARCHS_PCIE); do \
 	$(MAKE) $(EXTRA_VARS) KSRC=$(BT_LINUX_DIR)-$$i CC=$(TARGET_CC) && \
 	$(BT_STRIP) $(BT_STRIP_LIBOPTS) igb.ko && gzip -9 -f igb.ko && \
+	mkdir -p $(TARGET_DIR)/lib/modules/$(BT_KERNEL_RELEASE)-$$i/$(MODULE_PATH) && \
 	cp -a igb.ko.gz $(TARGET_DIR)/lib/modules/$(BT_KERNEL_RELEASE)-$$i/$(MODULE_PATH) ||\
 	exit 1 ; done)
 	-cp -a $(TARGET_DIR)/* $(BT_STAGING_DIR)
@@ -28,7 +27,8 @@ build: $(DIR)/.build
 
 clean:
 	rm -rf $(TARGET_DIR)
-#	$(MAKE) -C $(DIR) clean
+	(cd $(DIR)/src && for i in $(KARCHS_PCIE); do \
+		$(MAKE) $(EXTRA_VARS) KSRC=$(BT_LINUX_DIR)-$$i CC=$(TARGET_CC) clean ; done)
 	rm -f $(DIR)/.build
 
 srcclean: clean
