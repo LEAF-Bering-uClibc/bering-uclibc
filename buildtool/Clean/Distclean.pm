9cb228ad55be2461c46a9e278fd5a67819a4a37d
Yves Blusseau <blusseau@users.sourceforge.net>
2012-09-13 16:04:10 +0200

    Update buildtool::Clean::Distclean module
    * Use buildtool::Tools library

--- buildtool/Clean/Distclean.pm
+++ buildtool/Clean/Distclean.pm
@@ -6,6 +6,8 @@ package buildtool::Clean::Distclean;
 use strict;
 use Carp;
 
+use buildtool::Tools qw< remove_dir expand_variables >;
+
 use parent qw< buildtool::Common::Object >;
 
 sub clean () {
@@ -16,14 +18,14 @@ sub clean () {
     # remove the dirs in here...
     my @dirs = @{ $self->{'CONFIG'}{'buildenv_dir'} };
     foreach my $dir (@dirs) {
-        my $dir1 = $self->absoluteFilename($dir);
-        print "removing $dir1 recursive ";
-        if ( system("rm -rf $dir1") != 0 ) {
-            my $err = "" . $!;
-            $self->die( "removing $dir1 failed", $err );
+        if ($dir) {
+            my $dir1 = $self->absoluteFilename(
+                                  expand_variables( $dir, $self->{'CONFIG'} ) );
+            print "removing $dir1 recursive ";
+            remove_dir($dir1);
+            $self->print_ok();
+            print "\n";
         }
-        $self->print_ok();
-        print "\n";
     }
 
     # remove the installed packages file
@@ -32,8 +34,7 @@ sub clean () {
 
     # read in the file if it exists
     if ( -e $listfile ) {
-        system("rm -f $listfile") == 0
-          or die "removing $listfile failed: " . $!;
+        unlink $listfile or die "removing $listfile failed: " . $!;
     }
 
     # now at last remove the directory for the installedfiles
@@ -41,11 +42,14 @@ sub clean () {
       if (   !$self->{'CONFIG'}{'buildtracedir'}
            or $self->{'CONFIG'}{'buildtracedir'} eq "" );
 
-    system( "rm", "-rf", $self->{'CONFIG'}{'buildtracedir'} ) == 0
-      or
-      $self->die( "removing " . $self->{'CONFIG'}{'buildtracedir'} . "failed: ",
-                  "" . $! );
+    remove_dir(
+                $self->absoluteFilename(
+                           expand_variables(
+                                             $self->{'CONFIG'}{'buildtracedir'},
+                                             $self->{'CONFIG'}
+                                           )
+                                       )
+              );
 }
 
 1;
-
