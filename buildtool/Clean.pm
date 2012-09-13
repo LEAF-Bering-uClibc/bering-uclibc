71e6492e2b520cb8e3bdc58fe913280b0344db13
Yves Blusseau <blusseau@users.sourceforge.net>
2012-09-13 17:06:41 +0200

    Upgrade buildtool::Clean module
    * Use pure perl to remove files or directories

--- buildtool/Clean.pm
+++ buildtool/Clean.pm
@@ -1,69 +1,77 @@
-# $Id: Clean.pm,v 1.1.1.1 2010/04/26 09:03:17 nitr0man Exp $
 # This file is the base to be used for clean functions.
 #
 
 package buildtool::Clean;
+
+use strict;
+use Carp;
+
+use Config::General 2.15;
+use File::Path qw< remove_tree >;
 
 use buildtool::Common::InstalledFile;
 use buildtool::Clean::Buildclean;
 use buildtool::Clean::Remove;
 use buildtool::Clean::Distclean;
 use buildtool::Clean::Srcclean;
-use Config::General 2.15;
-use Carp;
-use strict;
 
-use vars qw(@ISA);
-
-@ISA = qw(buildtool::Common::InstalledFile);
-
+use parent qw< buildtool::Common::InstalledFile >;
 
 ###############################################################################
 # remove the package file list
 sub _removeFile() {
-  my $self = shift;
-  my $file = shift || return;
+    my $self = shift;
+    my $file = shift || return;
 
-  my $ret = system("rm", "-f", "$file");
-  if ($ret != 0) {
-    $self->debug("removing $file failed:$! ");
-    return 0;
-  } else {
-    $self->debug("file $file removed");
-    return 1;
-  }
+    my $ret = unlink "$file";
+    if ( $ret <= 0 ) {
+        $self->debug("removing $file failed:$!");
+        return 0;
+    } else {
+        $self->debug("file $file removed");
+        return 1;
+    }
 }
 
 ##########################################################################
 # remove the package file list
 sub _removeDir() {
-  my $self = shift;
-  my $dir = shift || return;
+    my $self = shift;
+    my $dir = shift || return;
 
-  my $ret = system("rmdir", "$dir");
-  if ($ret != 0) {
-    $self->debug("removing $dir failed:$! ");
-    return 0;
-  } else {
-    $self->debug("dir $dir removed");
+    if ( -d $dir ) {
+        my $ret = rmdir "$dir";
+        if (! $ret) {
+            $self->debug("removing $dir failed:$!");
+            return 0;
+        }
+        $self->debug("dir $dir removed");
+    }
     return 1;
-  }
 }
 
 ##########################################################################
 # force remove of dir
 sub _forceRemoveDir() {
-  my $self = shift;
-  my $dir = shift || return;
+    my $self = shift;
+    my $dir = shift || return;
 
-  my $ret = system("rm", "-rf", "$dir");
-  if ($ret != 0) {
-    $self->debug("force mode removing $dir failed:$! ");
-    return 0;
-  } else {
-    $self->debug("dir $dir removed in force mode");
+    if ( -d $dir ) {
+        remove_tree( $dir, { error => \my $err } );
+        if (@$err) {
+            for my $diag (@$err) {
+                my ( $dir, $message ) = %$diag;
+                if ( $dir eq '' ) {
+                    $self->debug("error:$message");
+                } else {
+                    $self->debug("force mode removing$dir failed:$message");
+                }
+            }
+            return 0;
+        }
+        $self->debug("dir $dir removed in force mode");
+    }
     return 1;
-  }
 }
 
 ###############################################################################
@@ -83,12 +91,10 @@ sub _removeFiles () {
   # reverse sort the dirlist by length - that way, we delete the child dirs
   # before trying to delete the parents
   foreach my $dir (sort { length($b) <=> length($a)} @{$dirlist}) {
-    die("removing directory $dir failed:") unless $self->_removeDir($dir);
+    die("removing directory $dir failed!") unless $self->_removeDir($dir);
   }
 
 }
-
-
 
 # END OF FILE
 1;
