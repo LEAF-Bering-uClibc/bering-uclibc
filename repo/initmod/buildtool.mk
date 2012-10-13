dde37bce61ae9b6aaeadfe389b020797f9567655
Andrew Denisenko <nitr0man@users.sourceforge.net>
2012-10-13 22:26:24 +0300

    Move initmod to separate package
    Now initrd and initmod are separate packages, and initrd is
    kernel-independent, so kernel for embedded devices can be built with
    embedded initrd into it.

---
+++ repo/initmod/buildtool.mk
@@ -0,0 +1,38 @@
+# makefile for initmod
+
+INITMOD_DIR=.
+ESCKEY=$(shell echo "a\nb"|awk '/\\n/ {print "-e"}')
+
+source:
+
+$(INITMOD_DIR)/.build:
+	-rm -f package.cfg
+	(for a in $(KARCHS); do \
+	sed 's,#.*$$,\n,' modulelist.common >modulelist.$$a ; \
+	[ -f specific.$$a ] && sed 's,#.*$$,\n,' specific.$$a >>modulelist.$$a ; \
+	BT_STAGING_DIR=$(BT_STAGING_DIR) BT_KERNEL_RELEASE=$(BT_KERNEL_RELEASE)-$$a \
+		    sh $(BT_TOOLS_DIR)/getdep.sh `cat modulelist.$$a` >mod ; \
+	[ -f files.$$a ] && rm -f files.$$a ; \
+	for m in `cat mod`; do echo $(ESCKEY) "<File>\n\tSource\t\t= lib/modules/__KVER__-$$a/$$m \n\t\
+	Filename\t= lib/modules/$$(echo $$m|sed 's,\([a-z0-9]*/\)\+,,')\n\t\
+	Type\t\t= binary\n\tType\t\t= module\n\tPermissions\t= 644\n</File>">>files.$$a; done; \
+	echo $(ESCKEY) "?include <common.$$a>" >>package.cfg; \
+	perl -p -e "s,##ARCH##,$$a,g" common.tpl >common.$$a ; \
+	done)
+
+	touch $(INITMOD_DIR)/.build
+
+build: $(INITMOD_DIR)/.build
+
+clean:
+	for a in $(KARCHS); do \
+		rm -f $(INITMOD_DIR)/modulelist.$$a ; \
+		rm -f $(INITMOD_DIR)/files.$$a ;      \
+		rm -f $(INITMOD_DIR)/common.$$a ;     \
+	done
+	rm -f $(INITMOD_DIR)/mod
+	rm -f $(INITMOD_DIR)/package.cfg
+	rm -f $(INITMOD_DIR)/.build
+
+srcclean: clean
+	rm -f $(INITMOD_DIR)/.source
--- repo/initrd/buildtool.mk
+++ repo/initrd/buildtool.mk
@@ -1,7 +1,7 @@
 # makefile for initrd
 
 INITRD_DIR=.
-INITRD_TARGET_DIR:=$(BT_BUILD_DIR)/config
+INITRD_TARGET_DIR:=$(BT_BUILD_DIR)/initrd
 ESCKEY=$(shell echo "a\nb"|awk '/\\n/ {print "-e"}')
 
 source:
@@ -13,19 +13,6 @@ $(INITRD_DIR)/.build:
 	mkdir -p $(INITRD_TARGET_DIR)/sbin
 
 	echo $(ESCKEY) "isofs\nvfat" > $(INITRD_TARGET_DIR)/boot/etc/modules
-	-rm -f package.cfg
-	(for a in $(KARCHS); do \
-	sed 's,#.*$$,\n,' modulelist.common >modulelist.$$a ; \
-	[ -f specific.$$a ] && sed 's,#.*$$,\n,' specific.$$a >>modulelist.$$a ; \
-	BT_STAGING_DIR=$(BT_STAGING_DIR) BT_KERNEL_RELEASE=$(BT_KERNEL_RELEASE)-$$a \
-		    sh $(BT_TOOLS_DIR)/getdep.sh `cat modulelist.$$a` >mod ; \
-	[ -f files.$$a ] && rm -f files.$$a ; \
-	for m in `cat mod`; do echo $(ESCKEY) "<File>\n\tSource\t\t= lib/modules/__KVER__-$$a/$$m \n\t\
-	Filename\t= lib/modules/$$(echo $$m|sed 's,\([a-z0-9]*/\)\+,,')\n\t\
-	Type\t\t= binary\n\tType\t\t= module\n\tPermissions\t= 644\n</File>">>files.$$a; done; \
-	echo $(ESCKEY) "?include <common.$$a>" >>package.cfg; \
-	perl -p -e "s,##ARCH##,$$a,g" common.tpl >common.$$a ; \
-	done)
 
 	cp -aL README $(INITRD_TARGET_DIR)/boot/etc
 	cp -aL root.linuxrc $(INITRD_TARGET_DIR)/var/lib/lrpkg
@@ -38,16 +25,7 @@ build: $(INITRD_DIR)/.build
 
 clean:
 	rm -rf $(INITRD_TARGET_DIR)
-	for a in $(KARCHS); do \
-		rm -f $(INITRD_DIR)/modulelist.$$a ; \
-		rm -f $(INITRD_DIR)/files.$$a ;      \
-		rm -f $(INITRD_DIR)/common.$$a ;     \
-	done
-	rm -f $(INITRD_DIR)/mod
-	rm -f $(INITRD_DIR)/package.cfg
 	rm -f $(INITRD_DIR)/.build
-	rm -f $(INITRD_DIR)/.configured
 
 srcclean: clean
 	rm -f $(INITRD_DIR)/.source
-	rm -f $(INITRD_DIR)/modules
