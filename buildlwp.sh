8954729326cde7fdd49a405ae8b984e8f5d3cca1
Yves Blusseau <blusseau@users.sourceforge.net>
2012-10-13 17:29:23 +0200

    Update buildlwp.sh script
    Use the buildtool environment variables

--- buildlwp.sh
+++ buildlwp.sh
@@ -3,75 +3,88 @@
 #
 # If necessary it adds the files with owner and permissions to a tarfile and then gzip's the tarfile
 
-ROOT=`pwd`
-TOOLCHAIN=`grep -i ^Toolchain= $ROOT/conf/buildtool.conf | cut -d= -f2`
-PKGDIR=package/$TOOLCHAIN
+set -u
 
-# change to lwp dir
-cd source/$TOOLCHAIN/lwp
+#########################################################
+BTROOTDIR=$(git rev-parse --show-toplevel 2>/dev/null || echo '.')
+BTBIN=$BTROOTDIR/buildtool.pl
+
+################## set environment ######################
+tmpfile=$(mktemp)
+$BTBIN dumpenv > $tmpfile || exit 1
+source $tmpfile || exit 1
+rm -f $tmpfile
+
+lwpSrcDir=$BT_SOURCE_DIR/lwp
 
 #
 # webconf.lwp
 #
-tar --owner=0 --group=0 --mode=755 -cf $ROOT/$PKGDIR/webconf.lwp var/webconf/www/lrcfg.cgi \
-	var/webconf/www/leafcfg.cgi \
-	etc/webconf/webconf-expert.webconf 
-tar --owner=1003 --group=65534 --mode=644 -rf $ROOT/$PKGDIR/webconf.lwp var/webconf/www/blurb.expert
-gzip -9 $ROOT/$PKGDIR/webconf.lwp 
-mv $ROOT/$PKGDIR/webconf.lwp.gz $ROOT/$PKGDIR/webconf.lwp 
-echo "webconf.lwp"
+tar -C $lwpSrcDir --owner=0 --group=0 --mode=755 \
+    -cf $BT_PACKAGE_DIR/webconf.lwp              \
+    var/webconf/www/lrcfg.cgi var/webconf/www/leafcfg.cgi etc/webconf/webconf-expert.webconf
 
+tar -C $lwpSrcDir --owner=1003 --group=65534 --mode=644 \
+    -rf $BT_PACKAGE_DIR/webconf.lwp var/webconf/www/blurb.expert
+gzip -9 $BT_PACKAGE_DIR/webconf.lwp
+mv $BT_PACKAGE_DIR/webconf.lwp.gz $BT_PACKAGE_DIR/webconf.lwp
+echo "webconf.lwp"
 
 #
 # keyboard.lwp
 #
-tar --owner=0 --group=0 --mode=755 -cf $ROOT/$PKGDIR/keyboard.lwp var/webconf/www/keyboard.cgi
-tar --owner=0 --group=0 --mode=644 -rf $ROOT/$PKGDIR/keyboard.lwp etc/webconf/keyboard.webconf
-gzip -9 $ROOT/$PKGDIR/keyboard.lwp 
-mv $ROOT/$PKGDIR/keyboard.lwp.gz $ROOT/$PKGDIR/keyboard.lwp 
+tar -C $lwpSrcDir --owner=0 --group=0 --mode=755 \
+    -cf $BT_PACKAGE_DIR/keyboard.lwp var/webconf/www/keyboard.cgi
+tar -C $lwpSrcDir --owner=0 --group=0 --mode=644 \
+    -rf $BT_PACKAGE_DIR/keyboard.lwp etc/webconf/keyboard.webconf
+gzip -9 $BT_PACKAGE_DIR/keyboard.lwp
+mv $BT_PACKAGE_DIR/keyboard.lwp.gz $BT_PACKAGE_DIR/keyboard.lwp
 echo "keyboard.lwp"
-	
+
 #
 # dropbear.lwp
 #
-tar --owner=0 --group=0 --mode=755 -cf $ROOT/$PKGDIR/dropbear.lwp var/webconf/www/dropbear.cgi
-tar --owner=0 --group=0 --mode=644 -rf $ROOT/$PKGDIR/dropbear.lwp etc/webconf/dropbear.webconf
-gzip -9 $ROOT/$PKGDIR/dropbear.lwp 
-mv $ROOT/$PKGDIR/dropbear.lwp.gz $ROOT/$PKGDIR/dropbear.lwp 
+tar -C $lwpSrcDir --owner=0 --group=0 --mode=755 \
+    -cf $BT_PACKAGE_DIR/dropbear.lwp var/webconf/www/dropbear.cgi
+tar -C $lwpSrcDir --owner=0 --group=0 --mode=644 \
+    -rf $BT_PACKAGE_DIR/dropbear.lwp etc/webconf/dropbear.webconf
+gzip -9 $BT_PACKAGE_DIR/dropbear.lwp
+mv $BT_PACKAGE_DIR/dropbear.lwp.gz $BT_PACKAGE_DIR/dropbear.lwp
 echo "dropbear.lwp"
 
 #
 # config.lwp
 #
-tar --owner=0 --group=0 --mode=755 -zcf $ROOT/$PKGDIR/config.lwp var/webconf/www/config.cgi \
-				etc/webconf/config.webconf
+tar -C $lwpSrcDir --owner=0 --group=0 --mode=755 \
+    -zcf $BT_PACKAGE_DIR/config.lwp var/webconf/www/config.cgi etc/webconf/config.webconf
 echo "config.lwp"
 
 #
 # dnsmasq.lwp
 #
-tar --owner=0 --group=0 --mode=755 -cf $ROOT/$PKGDIR/dnsmasq.lwp var/webconf/www/dnsmasq.cgi
-tar --owner=0 --group=0 --mode=644 -rf $ROOT/$PKGDIR/dnsmasq.lwp etc/webconf/dnsmasq.webconf
-gzip -9 $ROOT/$PKGDIR/dnsmasq.lwp 
-mv $ROOT/$PKGDIR/dnsmasq.lwp.gz $ROOT/$PKGDIR/dnsmasq.lwp 
+tar -C $lwpSrcDir --owner=0 --group=0 --mode=755 \
+    -cf $BT_PACKAGE_DIR/dnsmasq.lwp var/webconf/www/dnsmasq.cgi
+tar -C $lwpSrcDir --owner=0 --group=0 --mode=644 \
+    -rf $BT_PACKAGE_DIR/dnsmasq.lwp etc/webconf/dnsmasq.webconf
+gzip -9 $BT_PACKAGE_DIR/dnsmasq.lwp
+mv $BT_PACKAGE_DIR/dnsmasq.lwp.gz $BT_PACKAGE_DIR/dnsmasq.lwp
 echo "dnsmasq.lwp"
 
 #
 # pppoe.lwp
 #
-tar --owner=0 --group=0 --mode=755 -zcf $ROOT/$PKGDIR/pppoe.lwp	var/webconf/www/pppoe.cgi \
-				etc/webconf/pppoe.webconf
+tar -C $lwpSrcDir --owner=0 --group=0 --mode=755 \
+    -zcf $BT_PACKAGE_DIR/pppoe.lwp  var/webconf/www/pppoe.cgi etc/webconf/pppoe.webconf
 echo "pppoe.lwp"
 
 
 #
 # webipv6.lwp
 #
-tar --owner=0 --group=0 --mode=755 -cf $ROOT/$PKGDIR/webipv6.lwp var/webconf/www/webipv6.cgi
-tar --owner=0 --group=0 --mode=644 -rf $ROOT/$PKGDIR/webipv6.lwp etc/webconf/webipv6.webconf
-gzip -9 $ROOT/$PKGDIR/webipv6.lwp 
-mv $ROOT/$PKGDIR/webipv6.lwp.gz $ROOT/$PKGDIR/webipv6.lwp 
+tar -C $lwpSrcDir --owner=0 --group=0 --mode=755 \
+    -cf $BT_PACKAGE_DIR/webipv6.lwp var/webconf/www/webipv6.cgi
+tar -C $lwpSrcDir --owner=0 --group=0 --mode=644 \
+    -rf $BT_PACKAGE_DIR/webipv6.lwp etc/webconf/webipv6.webconf
+gzip -9 $BT_PACKAGE_DIR/webipv6.lwp
+mv $BT_PACKAGE_DIR/webipv6.lwp.gz $BT_PACKAGE_DIR/webipv6.lwp
 echo "webipv6.lwp"
-
-# go back
-cd $ROOT
