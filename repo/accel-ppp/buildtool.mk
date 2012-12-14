6bc97ce8d6e7d2035e3c8f2f5c43be630940621d
Andrew Denisenko <nitr0man@users.sourceforge.net>
2012-12-14 15:22:42 +0200

    accel-ppp: update to upstream version

--- repo/accel-ppp/buildtool.mk
+++ repo/accel-ppp/buildtool.mk
@@ -6,9 +6,7 @@ ACCEL_TARGET_DIR:=$(BT_BUILD_DIR)/accel-ppp
 $(ACCEL_DIR)/.source:
 	$(BT_SETUP_BUILDDIR) -v $(ACCEL_SOURCE)
 	perl -i -p -e 's,#include\s*\<printf.h\>,,' $(ACCEL_DIR)/accel-pppd/ctrl/pppoe/pppoe.c
-	cat $(PATCH1) | patch -p1 -d $(ACCEL_DIR)
-	cat $(PATCH2) | patch -p1 -d $(ACCEL_DIR)
-	cat $(PATCH3) | patch -p1 -d $(ACCEL_DIR)
+#	cat $(PATCH1) | patch -p1 -d $(ACCEL_DIR)
 	touch $@
 
 source: $(ACCEL_DIR)/.source
