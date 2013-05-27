ccf237401c144f2f5e31c86f2aad4bec136909a0
Andrew Denisenko <nitr0man@users.sourceforge.net>
2013-05-27 11:08:03 +0300

    iscsi: update to trunk
    Updated to trunk version from SVN to fix compile error (SVN has mostly
    bugfixes/fresh kernel supporting, so no regressions should be present)

--- repo/iscsi/buildtool.mk
+++ repo/iscsi/buildtool.mk
@@ -5,32 +5,34 @@
 #############################################################
 
 
-ISCSI_DIR:=iscsitarget-1.4.20.2
+ISCSI_DIR:=$(shell $(BT_TGZ_GETDIRNAME) $(ISCSI_SOURCE) 2>/dev/null )
 ISCSI_TARGET_DIR:=$(BT_BUILD_DIR)/iscsi
 
 source:
 	-rm -rf $(ISCSI_DIR)
 	zcat $(ISCSI_SOURCE) | tar -xvf -
-	cat $(ISCSI_PATCH1) | patch -d $(ISCSI_DIR) -p0
-	cat $(ISCSI_PATCH2) | patch -d $(ISCSI_DIR) -p0
-	cat $(ISCSI_PATCH3) | patch -d $(ISCSI_DIR) -p1
-	cat $(ISCSI_PATCH4) | patch -d $(ISCSI_DIR) -p0
-	cat $(ISCSI_PATCH5) | patch -d $(ISCSI_DIR) -p1
-	cat $(ISCSI_PATCH6) | patch -d $(ISCSI_DIR) -p0
-	cat $(ISCSI_PATCH7) | patch -d $(ISCSI_DIR) -p0
-	cat $(ISCSI_PATCH8) | patch -d $(ISCSI_DIR) -p1
+#	cat $(ISCSI_PATCH1) | patch -d $(ISCSI_DIR) -p0
+#	cat $(ISCSI_PATCH2) | patch -d $(ISCSI_DIR) -p0
+#	cat $(ISCSI_PATCH3) | patch -d $(ISCSI_DIR) -p1
+#	cat $(ISCSI_PATCH4) | patch -d $(ISCSI_DIR) -p0
+#	cat $(ISCSI_PATCH5) | patch -d $(ISCSI_DIR) -p1
+#	cat $(ISCSI_PATCH6) | patch -d $(ISCSI_DIR) -p0
+#	cat $(ISCSI_PATCH7) | patch -d $(ISCSI_DIR) -p0
+#	cat $(ISCSI_PATCH8) | patch -d $(ISCSI_DIR) -p1
 
 build:
 	mkdir -p $(ISCSI_TARGET_DIR)/usr/sbin
 	mkdir -p $(ISCSI_TARGET_DIR)/etc/init.d
 	mkdir -p $(ISCSI_TARGET_DIR)/etc/modules.d
+	$(MAKE) $(MAKEOPTS) -C $(ISCSI_DIR) CC=$(TARGET_CC) usr
 	(for i in $(KARCHS); do \
 	mkdir -p $(BT_STAGING_DIR)/lib/modules/$(BT_KERNEL_RELEASE)-$$i/kernel/iscsi && \
-	$(MAKE) $(MAKEOPTS) KSRC=$(BT_LINUX_DIR)-$$i CC=$(TARGET_CC) -C $(ISCSI_DIR) && \
+	$(MAKE) $(MAKEOPTS) KSRC=$(BT_LINUX_DIR)-$$i -C $(ISCSI_DIR) CC=$(TARGET_CC) unpatch && \
+	$(MAKE) $(MAKEOPTS) KSRC=$(BT_LINUX_DIR)-$$i -C $(ISCSI_DIR) CC=$(TARGET_CC) kernel && \
 	gzip -9 $(ISCSI_DIR)/kernel/iscsi_trgt.ko && \
 	cp -a $(ISCSI_DIR)/kernel/iscsi_trgt.ko.gz $(BT_STAGING_DIR)/lib/modules/$(BT_KERNEL_RELEASE)-$$i/kernel/iscsi && \
 	rm -f $(ISCSI_DIR)/kernel/iscsi_trgt.ko.gz && \
-	depmod -ae -b $(BT_STAGING_DIR) -r -F $(BT_STAGING_DIR)/lib/modules/$(BT_KERNEL_RELEASE)-$$i/build/System.map $(BT_KERNEL_RELEASE)-$$i ; \
+	depmod -ae -b $(BT_STAGING_DIR) -r -F $(BT_STAGING_DIR)/lib/modules/$(BT_KERNEL_RELEASE)-$$i/build/System.map $(BT_KERNEL_RELEASE)-$$i || exit 1; \
 	done)
 	cp -a $(ISCSI_DIR)/usr/ietd $(ISCSI_TARGET_DIR)/usr/sbin
 	cp -a $(ISCSI_DIR)/usr/ietadm $(ISCSI_TARGET_DIR)/usr/sbin
@@ -47,4 +49,4 @@ clean:
 	rm -f $(BT_STAGING_DIR)/usr/sbin/ietadm
 	rm -f $(BT_STAGING_DIR)/etc/init.d/iscsi-target
 	rm -f $(BT_STAGING_DIR)/etc/ietd.conf
-	rm -f $(BT_STAGING_DIR)/lib/modules/$(BT_KERNEL_RELEASE)/kernel/iscsi/iscsi_trgt.o
+	rm -f $(BT_STAGING_DIR)/lib/modules/$(BT_KERNEL_RELEASE)/kernel/iscsi/iscsi_trgt.ko.gz
