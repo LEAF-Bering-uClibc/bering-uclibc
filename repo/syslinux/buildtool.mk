0f11ab614cfda1b18cb0d7af6ac9bff775e32b65
kapeka <kapeka@users.sourceforge.net>
2013-07-28 17:48:50 +0200

    syslinux move menu.c32, vesamenu.c32 and hdt.c32 into staging for later use.
    These files are needed if we move to a new bootmenu.

--- repo/syslinux/buildtool.mk
+++ repo/syslinux/buildtool.mk
@@ -14,12 +14,18 @@ $(SYSLINUX_DIR)/.source:
 $(SYSLINUX_DIR)/.build: $(SYSLINUX_DIR)/.source
 	mkdir -p $(SYSLINUX_TARGET_DIR)/usr/bin
 	mkdir -p $(SYSLINUX_TARGET_DIR)/usr/share/syslinux
+	mkdir -p $(SYSLINUX_TARGET_DIR)/com32/menu
 	$(MAKE) $(MAKEOPTS) CC=$(TARGET_CC) -C $(SYSLINUX_DIR) installer
 	cp -a $(SYSLINUX_DIR)/core/pxelinux.0 $(SYSLINUX_TARGET_DIR)/usr/share/syslinux/
 	cp -a $(SYSLINUX_DIR)/core/isolinux.bin $(SYSLINUX_TARGET_DIR)/usr/share/syslinux/
 	cp -a $(SYSLINUX_DIR)/gpxe/gpxelinux.0 $(SYSLINUX_TARGET_DIR)/usr/share/syslinux/
 	cp -a $(SYSLINUX_DIR)/mtools/syslinux $(SYSLINUX_TARGET_DIR)/usr/bin/
 	cp -a $(SYSLINUX_DIR)/mbr/mbr.bin $(SYSLINUX_TARGET_DIR)/usr/share/syslinux/
+	cp -a $(SYSLINUX_DIR)/com32/menu/vesamenu.c32 $(SYSLINUX_TARGET_DIR)/com32/menu
+	cp -a $(SYSLINUX_DIR)/com32/menu/menu.c32 $(SYSLINUX_TARGET_DIR)/com32/menu
+
+	cp -a $(SYSLINUX_DIR)/com32/hdt/hdt.c32 $(SYSLINUX_TARGET_DIR)/com32/menu
+
 	-$(BT_STRIP) $(BT_STRIP_BINOPTS) $(SYSLINUX_TARGET_DIR)/usr/bin/*
 	cp -a $(SYSLINUX_TARGET_DIR)/* $(BT_STAGING_DIR)
 	touch $(SYSLINUX_DIR)/.build
