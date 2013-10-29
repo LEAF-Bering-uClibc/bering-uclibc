be7095d5a57ed3f3ce0d301e5080e76d7349258c
kapeka <kapeka@users.sourceforge.net>
2013-10-29 19:19:03 +0100

    fix ipvsadm compilation with libpopt

--- repo/ipvsadm/buildtool.mk
+++ repo/ipvsadm/buildtool.mk
@@ -1,50 +1,38 @@
-#############################################################
-#
-# ipvsadm
-#
-#############################################################
+# makefile for pump
+
+PUMP_DIR:=$(shell $(BT_TGZ_GETDIRNAME) $(PUMP_SOURCE) 2>/dev/null )
+PUMP_TARGET_DIR:=$(BT_BUILD_DIR)/pump
+
+export LDFLAGS += $(EXTCCLDFLAGS)
+
+$(PUMP_DIR)/.source:
+	tar xvzf $(PUMP_SOURCE)
+	zcat $(PUMP_PATCH1) | patch -d $(PUMP_DIR) -p1
+	perl -i -p -e 's,-Wl\S+,,g' $(PUMP_DIR)/Makefile
+	touch $(PUMP_DIR)/.source
+
+source: $(PUMP_DIR)/.source
+
+$(PUMP_DIR)/.build: $(PUMP_DIR)/.source
+	mkdir -p $(PUMP_TARGET_DIR)
+	mkdir -p $(PUMP_TARGET_DIR)/etc/
+	mkdir -p $(PUMP_TARGET_DIR)/sbin
+	make $(MAKEOPTS) -C $(PUMP_DIR) CC=$(TARGET_CC) LDFLAGS="$(LDFLAGS)" \
+	DEB_CFLAGS="$(CFLAGS)" pump
+	cp -a $(PUMP_DIR)/pump $(PUMP_TARGET_DIR)/sbin
+	-$(BT_STRIP) $(BT_STRIP_BINOPTS) $(PUMP_TARGET_DIR)/sbin/*
+	cp -aL pump.shorewall $(PUMP_TARGET_DIR)/etc
+	cp -aL pump.conf $(PUMP_TARGET_DIR)/etc
+	cp -a $(PUMP_TARGET_DIR)/* $(BT_STAGING_DIR)
+	touch $(PUMP_DIR)/.build
 
 
-IPVSADM_DIR:=$(shell $(BT_TGZ_GETDIRNAME) $(IPVSADM_SOURCE) 2>/dev/null )
-IPVSADM_TARGET_DIR:=$(BT_BUILD_DIR)/ipvsadm
-
-KERNELSOURCE = $(BT_LINUX_DIR)
-
-$(IPVSADM_DIR)/.source:
-	tar xvzf $(IPVSADM_SOURCE)
-	# specify use of target (rather than host) strip program for "install"
-	cat $(IPVSADM_LIBNL3) | patch -d $(IPVSADM_DIR) -p1
-	perl -i -p -e "s, -s ipvsadm , -s --strip-program=$(GNU_TARGET_NAME)-strip ipvsadm ," $(IPVSADM_DIR)/Makefile
-	touch $(IPVSADM_DIR)/.source
-
-source: $(IPVSADM_DIR)/.source
-
-$(IPVSADM_DIR)/.build: $(IPVSADM_DIR)/.source
-	mkdir -p $(IPVSADM_TARGET_DIR)
-	mkdir -p $(IPVSADM_TARGET_DIR)/etc/
-	mkdir -p $(IPVSADM_TARGET_DIR)/etc/init.d
-	mkdir -p $(IPVSADM_TARGET_DIR)/etc/default
-	mkdir -p $(IPVSADM_TARGET_DIR)/sbin
-	#Use single-thread make due to poor makefile
-	make -C $(IPVSADM_DIR) \
-		LIB_SEARCH="$(BT_STAGING_DIR)/usr/lib $(BT_STAGING_DIR)/lib" \
-		CC=$(TARGET_CC) \
-		BUILD_ROOT=$(IPVSADM_TARGET_DIR) \
-		CFLAGS="$(CFLAGS) $(LDFLAGS) -fPIC" KERNELSOURCE=$(KERNELSOURCE) install
-	-rm -rf $(IPVSADM_TARGET_DIR)/usr/man
-	-$(BT_STRIP) $(BT_STRIP_BINOPTS) $(IPVSADM_TARGET_DIR)/sbin/*
-	cp -aL ipvsadm.default $(IPVSADM_TARGET_DIR)/etc/default/ipvsadm
-	cp -aL ipvsadm.rules $(IPVSADM_TARGET_DIR)/etc
-	cp -aL ipvsadm.init $(IPVSADM_TARGET_DIR)/etc/init.d/ipvsadm
-	cp -a $(IPVSADM_TARGET_DIR)/* $(BT_STAGING_DIR)
-	touch $(IPVSADM_DIR)/.build
-
-build: $(IPVSADM_DIR)/.build
+build: $(PUMP_DIR)/.build
 
 clean:
-	make -C $(IPVSADM_DIR) clean
-	rm -rf $(IPVSADM_DIR)/.build
-	rm -rf $(IPVSADM_DIR)/.configured
+	make -C $(PUMP_DIR) clean
+	rm -rf $(PUMP_DIR)/.build
+	rm -rf $(PUMP_DIR)/.configured
 
 srcclean: clean
-	rm -rf $(IPVSADM_DIR)
+	rm -rf $(PUMP_DIR)
