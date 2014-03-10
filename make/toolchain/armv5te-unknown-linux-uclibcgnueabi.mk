420c5a43a82e8f936e89d57f0e0d8ed427c107e0
kapeka <kapeka@users.sourceforge.net>
2014-03-10 18:05:52 +0100

    kernl: updated to upstream version 3.10.33

--- make/toolchain/armv5te-unknown-linux-uclibcgnueabi.mk
+++ make/toolchain/armv5te-unknown-linux-uclibcgnueabi.mk
@@ -5,7 +5,7 @@
 ifeq ($(GNU_TARGET_NAME),armv5te-unknown-linux-uclibcgnueabi)
 # Kernel versions
 export BT_KERNEL_BRANCH:=3.10
-export BT_KERNEL_PATCH:=29
+export BT_KERNEL_PATCH:=33
 # Primary kernel architecture
 export ARCH:=arm
 # Arch for includes symlink
--- make/toolchain/i486-unknown-linux-uclibc.mk
+++ make/toolchain/i486-unknown-linux-uclibc.mk
@@ -5,7 +5,7 @@
 ifeq ($(GNU_TARGET_NAME),i486-unknown-linux-uclibc)
 # Kernel versions
 export BT_KERNEL_BRANCH:=3.10
-export BT_KERNEL_PATCH:=29
+export BT_KERNEL_PATCH:=33
 # Primary kernel architecture
 export ARCH:=i386
 # Arch for includes symlink
