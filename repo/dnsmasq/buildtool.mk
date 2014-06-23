b1093538460dc8e9f5a1358312455b6bf1123f18
kapeka <kapeka@users.sourceforge.net>
2014-06-23 20:51:23 +0200

    dnsmasq : build two packages with and without DNSSEC support
    dnsmasq.lrp is the known package
    dnsmasqsec.lrp is the package with DNSSEC support
    
    dnsmasqsec.lrp requires libgmp and libnettle
    
    To enable and test DNSSEC support From dnsmasq mailinglist:
    "So, adding
     conf-file=/path/to/trust-anchors.conf
     dnssec
    to your config is all thats needed to get things
    working. The upstream nameservers have to be DNSSEC-capable
    too, of course. Many ISP nameservers aren't, but the
    Google public nameservers (8.8.8.8 and 8.8.4.4) are."
    
    Enable log-queries to show results.
    
    Point your browser to
     paypal.com
     ietf.org
     www.dnssec-failed.org
    
    and see how/if it works.

--- repo/dnsmasq/buildtool.mk
+++ repo/dnsmasq/buildtool.mk
@@ -16,11 +16,9 @@ $(DNSMASQ_DIR)/.build: $(DNSMASQ_DIR)/.source
 	mkdir -p $(DNSMASQ_TARGET_DIR)/usr/sbin
 	mkdir -p $(DNSMASQ_TARGET_DIR)/etc/init.d
 
-#	cp Makefile.dnsmasq $(DNSMASQ_DIR)/Makefile
-
 # Disable the internal tftpd with -DNO_TFTP
 	make $(MAKEOPTS) -C $(DNSMASQ_DIR) DESTDIR=$(DNSMASQ_TARGET_DIR) \
-	CC=$(TARGET_CC) LD=$(TARGET_LD) CFLAGS="$(CFLAGS) -Wall -w -DHAVE_DNSSEC -DNO_TFTP" LDFLAGS="$(LDFLAGS) -lnettle -lgmp -lhogweed" all
+	CC=$(TARGET_CC) LD=$(TARGET_LD) CFLAGS="$(CFLAGS) -Wall -w -DNO_TFTP" LDFLAGS="$(LDFLAGS)" all
 # Adjust the dnsmasq.conf to LEAF standard
 	perl -i -p -e 's,\#dhcp-range=192.168.0.50\,192.168.0.150\,12h,\#dhcp-range=192.168.1.1\,192.168.1.199\,12h,g' $(DNSMASQ_DIR)/dnsmasq.conf.example
 	perl -i -p -e 's,\#domain=thekelleys.org.uk,domain=private.network,g' $(DNSMASQ_DIR)/dnsmasq.conf.example
@@ -28,8 +26,17 @@ $(DNSMASQ_DIR)/.build: $(DNSMASQ_DIR)/.source
 	cp -aL dnsmasq.init $(DNSMASQ_TARGET_DIR)/etc/init.d/dnsmasq
 	cp -a $(DNSMASQ_DIR)/src/dnsmasq $(DNSMASQ_TARGET_DIR)/usr/sbin
 	cp -a $(DNSMASQ_DIR)/dnsmasq.conf.example $(DNSMASQ_TARGET_DIR)/etc/dnsmasq.conf
+	cp -a $(DNSMASQ_DIR)/trust-anchors.conf $(DNSMASQ_TARGET_DIR)/etc/
 	-$(BT_STRIP) $(BT_STRIP_BINOPTS) $(DNSMASQ_TARGET_DIR)/usr/sbin/*
 	cp -a $(DNSMASQ_TARGET_DIR)/* $(BT_STAGING_DIR)
+
+# build dnsmasq with dnssec support
+	make -C $(DNSMASQ_DIR) clean
+	make $(MAKEOPTS) -C $(DNSMASQ_DIR) DESTDIR=$(DNSMASQ_TARGET_DIR) \
+	CC=$(TARGET_CC) LD=$(TARGET_LD) CFLAGS="$(CFLAGS) -Wall -w -DHAVE_DNSSEC -DNO_TFTP" LDFLAGS="$(LDFLAGS) -lnettle -lgmp -lhogweed" all
+	cp -a $(DNSMASQ_DIR)/src/dnsmasq $(DNSMASQ_TARGET_DIR)/usr/sbin/dnsmasqsec
+	-$(BT_STRIP) $(BT_STRIP_BINOPTS) $(DNSMASQ_TARGET_DIR)/usr/sbin/dnsmasqsec
+	cp -a $(DNSMASQ_TARGET_DIR)/usr/sbin/dnsmasqsec $(BT_STAGING_DIR)/usr/sbin
 	touch $(DNSMASQ_DIR)/.build
 
 build: $(DNSMASQ_DIR)/.build
