b95e6b485fc9e23da02133f4959b2545ca0349eb
kapeka <kapeka@users.sourceforge.net>
2014-07-03 18:02:36 +0200

    mpg123 : re-add package with improved buildtool setup
    May need more work and optimization, but it's a start and I won't see the work so far lost or doubled

--- repo/mpg123/buildtool.mk
+++ repo/mpg123/buildtool.mk
@@ -1,36 +1,78 @@
+#############################
+# makefile for mpg123
+##############################
 
-MPG123_DIR:=mpg123-0.60
+MPG123_DIR:=$(CURDIR)/$(shell $(BT_TGZ_GETDIRNAME) $(MPG123_SOURCE) 2>/dev/null )
+MPG123_TARGET_DIR:=$(BT_BUILD_DIR)/mpg123
 
 $(MPG123_DIR)/.source:
-	zcat $(MPG123_SOURCE) | tar -xvf -
+	$(BT_SETUP_BUILDDIR) -v $(MPG123_SOURCE)
 	touch $(MPG123_DIR)/.source
 
 $(MPG123_DIR)/.build: $(MPG123_DIR)/.source
-	mkdir -p $(BT_STAGING_DIR)/usr/bin
-	(cd $(MPG123_DIR); CFLAGS="$(BT_COPT_FLAGS)" CC=$(TARGET_CC) \
-		./configure --with-audio=oss --disable-debug --prefix=/usr \
-			--with-optimization=0 --with-cpu=i486)
+	mkdir -p $(MPG123_TARGET_DIR)/usr/bin
+	mkdir -p $(MPG123_TARGET_DIR)/usr/lib
+
+# optimized for i486
+	(cd $(MPG123_DIR); CFLAGS="$(CFLAGS)" CC=$(TARGET_CC) \
+	./configure \
+	--host=$(GNU_TARGET_NAME) \
+	--build=$(GNU_BUILD_NAME) \
+	--with-audio=oss \
+	--disable-debug \
+	--enable-network \
+	--prefix=/usr \
+	--with-cpu=i486) 
 	$(MAKE) -C $(MPG123_DIR)
-	cp -a -f $(MPG123_DIR)/src/mpg123 $(BT_STAGING_DIR)/usr/bin/mpg123_i486
+	cp -a -f $(MPG123_DIR)/src/.libs/mpg123 $(MPG123_TARGET_DIR)/usr/bin/mpg123_i486
+	cp -a -f $(MPG123_DIR)/src/.libs/mpg123-* $(MPG123_TARGET_DIR)/usr/bin/
+	cp -a -f $(MPG123_DIR)/src/.libs/out123 $(MPG123_TARGET_DIR)/usr/bin/
+	cp -a -f $(MPG123_DIR)/src/libmpg123/.libs/libmpg123.so.0.40.3 $(MPG123_TARGET_DIR)/usr/lib/libmpg123.so.0.40.3_i486
 	$(MAKE) -C $(MPG123_DIR) clean
-	(cd $(MPG123_DIR); CFLAGS="$(BT_COPT_FLAGS)" CC=$(TARGET_CC) \
-		./configure --with-audio=oss --disable-debug --prefix=/usr \
-			--with-optimization=0 --with-cpu=i586)
+
+# optimized for pentium
+	(cd $(MPG123_DIR); CFLAGS="$(CFLAGS)" CC=$(TARGET_CC) \
+	./configure --with-audio=oss \
+	--host=$(GNU_TARGET_NAME) \
+	--build=$(GNU_BUILD_NAME) \
+	--disable-debug \
+	--enable-network \
+	--prefix=/usr \
+	--with-cpu=i586)
 	$(MAKE) -C $(MPG123_DIR)
-	cp -a -f $(MPG123_DIR)/src/mpg123 $(BT_STAGING_DIR)/usr/bin/mpg123_pentium
+	cp -a -f $(MPG123_DIR)/src/.libs/mpg123 $(MPG123_TARGET_DIR)/usr/bin/mpg123_pentium
+	cp -a -f $(MPG123_DIR)/src/libmpg123/.libs/libmpg123.so.0.40.3 $(MPG123_TARGET_DIR)/usr/lib/libmpg123.so.0.40.3_pentium
 	$(MAKE) -C $(MPG123_DIR) clean
-	(cd $(MPG123_DIR); CFLAGS="$(BT_COPT_FLAGS)" CC=$(TARGET_CC) \
-		./configure --with-audio=oss --disable-debug --prefix=/usr \
-			--with-optimization=0 --with-cpu=3dnow)
+
+# optimized for 3dnow
+	(cd $(MPG123_DIR); CFLAGS="$(CFLAGS)" CC=$(TARGET_CC) \
+	./configure --with-audio=oss \
+	--host=$(GNU_TARGET_NAME) \
+	--build=$(GNU_BUILD_NAME) \
+	--disable-debug \
+	--enable-network \
+	--prefix=/usr \
+	--with-cpu=3dnow)
 	$(MAKE) -C $(MPG123_DIR)
-	cp -a -f $(MPG123_DIR)/src/mpg123 $(BT_STAGING_DIR)/usr/bin/mpg123_3dnow
+	cp -a -f $(MPG123_DIR)/src/.libs/mpg123 $(MPG123_TARGET_DIR)/usr/bin/mpg123_3dnow
+	cp -a -f $(MPG123_DIR)/src/libmpg123/.libs/libmpg123.so.0.40.3 $(MPG123_TARGET_DIR)/usr/lib/libmpg123.so.0.40.3_3dnow
 	$(MAKE) -C $(MPG123_DIR) clean
-	(cd $(MPG123_DIR); CFLAGS="$(BT_COPT_FLAGS)" CC=$(TARGET_CC) \
-		./configure --with-audio=oss --disable-debug --prefix=/usr \
-			--with-optimization=0 --with-cpu=mmx)
+
+# optimized for mmx
+	(cd $(MPG123_DIR); CFLAGS="$(CFLAGS)" CC=$(TARGET_CC) \
+	./configure --with-audio=oss \
+	--host=$(GNU_TARGET_NAME) \
+	--build=$(GNU_BUILD_NAME) \
+	--disable-debug \
+	--enable-network \
+	--prefix=/usr \
+	--with-cpu=mmx)
 	$(MAKE) -C $(MPG123_DIR)
-	cp -a -f $(MPG123_DIR)/src/mpg123 $(BT_STAGING_DIR)/usr/bin/mpg123_mmx
-	$(BT_STRIP) $(BT_STRIP_BINOPTS) $(BT_STAGING_DIR)/usr/bin/mpg123_*
+	cp -a -f $(MPG123_DIR)/src/.libs/mpg123 $(MPG123_TARGET_DIR)/usr/bin/mpg123_mmx
+	cp -a -f $(MPG123_DIR)/src/libmpg123/.libs/libmpg123.so.0.40.3 $(MPG123_TARGET_DIR)/usr/lib/libmpg123.so.0.40.3_mmx
+	cp -a $(MPG123_TARGET_DIR)/* $(BT_STAGING_DIR)
+	-$(BT_STRIP) $(BT_STRIP_BINOPTS) $(BT_STAGING_DIR)/usr/bin/mpg123_*
+	-$(BT_STRIP) $(BT_STRIP_BINOPTS) $(BT_STAGING_DIR)/usr/lib/libmpg123_*
 	touch $(MPG123_DIR)/.build
 
 source: $(MPG123_DIR)/.source
@@ -41,6 +83,8 @@ clean:
 	-rm $(MPG123_DIR)/.build
 	$(MAKE) -C $(MPG123_DIR) clean
 	rm -f $(BT_STAGING_DIR)/usr/bin/mpg123_*
+	rm -f $(BT_STAGING_DIR)/usr/lib/libmpg123*
+	rm -rf $(MPG123_TARGET_DIR)/usr/*
 
-srcclean:
+srcclean: clean
 	rm -rf $(MPG123_DIR)
